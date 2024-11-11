6a1baa944acc03927b3382c77dae4d945a2c4f10
Timepunk <45543880+0xTimepunk@users.noreply.github.com>
2024-11-11 18:56:52 +0700

    fix: deployments

--- contracts/script/DeployV2Core.s.sol
+++ contracts/script/DeployV2Core.s.sol
@@ -21,6 +21,7 @@ contract DeployV2Core is Script {
     address public constant CREATE3 = 0xd468A8f2f0CF8c905FC95dE67a94C1f8feaa3c8B;
     address public constant DEPLOYER = 0x48aB8AdF869Ba9902Ad483FB1Ca2eFDAb6eabe92;
     address public constant RELAYER = 0x8C91d7EADfFc9F9921092cA14C3e498cD8cDe0d3;
+    address public constant WETH = 0x4200000000000000000000000000000000000006;
 
     function deploy(uint64 chainId) public {
         vm.startBroadcast();
@@ -29,20 +30,19 @@ contract DeployV2Core is Script {
         deployer = Create3Deployer(CREATE3);
 
         address superRbac = deployer.deploy(
-            keccak256("SUPER_RBAC_V2"), abi.encodePacked(type(SuperRbac).creationCode, abi.encode(DEPLOYER))
+            keccak256("SUPER_RBAC_V3"), abi.encodePacked(type(SuperRbac).creationCode, abi.encode(DEPLOYER))
         );
 
         address superRegistry = deployer.deploy(
-            keccak256("SUPER_REGISTRY_V2"), abi.encodePacked(type(SuperRegistry).creationCode, abi.encode(DEPLOYER))
+            keccak256("SUPER_REGISTRY_V3"), abi.encodePacked(type(SuperRegistry).creationCode, abi.encode(DEPLOYER))
         );
 
         address relayerSentinel = deployer.deploy(
-            keccak256("RELAYER_SENTINEL_V2"),
+            keccak256("RELAYER_SENTINEL_V3"),
             abi.encodePacked(type(RelayerSentinel).creationCode, abi.encode(superRegistry))
         );
 
         SuperRbac superRbacContract = SuperRbac(superRbac);
-        console2.log("SuperRbac owner:", superRbacContract.owner());
         superRbacContract.setRole(DEPLOYER, superRbacContract.ADMIN_ROLE(), true);
         superRbacContract.setRole(DEPLOYER, superRbacContract.HOOK_REGISTRATION_ROLE(), true);
         superRbacContract.setRole(DEPLOYER, superRbacContract.HOOK_EXECUTOR_ROLE(), true);
@@ -57,26 +57,21 @@ contract DeployV2Core is Script {
 
         address deposit4626MintSuperPositionsDecoder;
         address deposit4626Module;
-        address weth;
         address superformVault;
         address superPositions;
-        // just for SuperTHAI
+        // just for Base Sepolia
         if (chainId == 0) {
-            weth = deployer.deploy(
-                keccak256("WETH_V2"), abi.encodePacked(type(ERC20Mock).creationCode, abi.encode("WETH", "WETH", 18))
-            );
-
             superformVault = deployer.deploy(
-                keccak256("SUPERFORM_VAULT_V2"),
-                abi.encodePacked(type(SuperformVault).creationCode, abi.encode(IERC20(weth), "SuperWETHVault", "sWETH"))
+                keccak256("SUPERFORM_VAULT_V3"),
+                abi.encodePacked(type(SuperformVault).creationCode, abi.encode(IERC20(WETH), "SuperWETHVault", "sWETH"))
             );
 
             deposit4626MintSuperPositionsDecoder = deployer.deploy(
-                keccak256("DEPOSIT_4626_MINT_SUPER_POSITIONS_DECODER_V2"),
+                keccak256("DEPOSIT_4626_MINT_SUPER_POSITIONS_DECODER_V3"),
                 abi.encodePacked(type(Deposit4626MintSuperPositionsDecoder).creationCode)
             );
             deposit4626Module = deployer.deploy(
-                keccak256("DEPOSIT_4626_MODULE_V2"),
+                keccak256("DEPOSIT_4626_MODULE_V3"),
                 abi.encodePacked(
                     type(Deposit4626Module).creationCode,
                     abi.encode(superRegistry, deposit4626MintSuperPositionsDecoder)
@@ -87,7 +82,7 @@ contract DeployV2Core is Script {
         } else if (chainId == 1) {
             // just for SuperChain
             superPositions = deployer.deploy(
-                keccak256("SUPER_POSITIONS_V2"),
+                keccak256("SUPER_POSITIONS_V3"),
                 abi.encodePacked(type(SuperPositions).creationCode, abi.encode(superRegistry, 18))
             );
             superRegistryContract.setAddress(superRegistryContract.SUPER_POSITIONS_ID(), superPositions);
@@ -98,7 +93,6 @@ contract DeployV2Core is Script {
         console2.log("SuperRegistry deployed to:", superRegistry);
         console2.log("RelayerSentinel deployed to:", relayerSentinel);
         if (chainId == 0) {
-            console2.log("WETH deployed to:", weth);
             console2.log("SuperformVault deployed to:", superformVault);
             console2.log("Deposit4626MintSuperPositionsDecoder deployed to:", deposit4626MintSuperPositionsDecoder);
             console2.log("Deposit4626Module deployed to:", deposit4626Module);
