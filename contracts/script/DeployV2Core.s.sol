e0439d023791aaaf87c0c3dbad4f8ce6fc449a20
Timepunk <45543880+0xTimepunk@users.noreply.github.com>
2024-11-11 13:46:19 +0700

    feat: contracts deployed

--- contracts/script/DeployV2Core.s.sol
+++ contracts/script/DeployV2Core.s.sol
@@ -15,28 +15,42 @@ import { Deposit4626Module } from "src/modules/Deposit4626Module.sol";
 contract DeployV2Core is Script {
     // Add storage variables
     Create3Deployer public deployer;
-    address public constant DEPLOYER = 0xd468A8f2f0CF8c905FC95dE67a94C1f8feaa3c8B;
+    address public constant CREATE3 = 0xd468A8f2f0CF8c905FC95dE67a94C1f8feaa3c8B;
+    address public constant DEPLOYER = 0x48aB8AdF869Ba9902Ad483FB1Ca2eFDAb6eabe92;
     address public constant RELAYER = 0x8C91d7EADfFc9F9921092cA14C3e498cD8cDe0d3;
-    address public constant ZERO = 0x0000000000000000000000000000000000000000;
 
     function deploy(uint64 chainId) public {
         vm.startBroadcast();
 
         // Initialize Create3Deployer
-        deployer = Create3Deployer(DEPLOYER);
+        deployer = Create3Deployer(CREATE3);
 
         address superRbac = deployer.deploy(
-            keccak256("SUPER_RBAC_V1"), abi.encodePacked(type(SuperRbac).creationCode, abi.encode(ZERO))
+            keccak256("SUPER_RBAC_V1"), abi.encodePacked(type(SuperRbac).creationCode, abi.encode(DEPLOYER))
         );
 
         address superRegistry = deployer.deploy(
-            keccak256("SUPER_REGISTRY_V1"), abi.encodePacked(type(SuperRegistry).creationCode, abi.encode(ZERO))
+            keccak256("SUPER_REGISTRY_V1"), abi.encodePacked(type(SuperRegistry).creationCode, abi.encode(DEPLOYER))
         );
 
         address relayerSentinel = deployer.deploy(
             keccak256("RELAYER_SENTINEL_V1"),
             abi.encodePacked(type(RelayerSentinel).creationCode, abi.encode(superRegistry))
         );
+
+        SuperRbac superRbacContract = SuperRbac(superRbac);
+        console2.log("SuperRbac owner:", superRbacContract.owner());
+        superRbacContract.setRole(DEPLOYER, superRbacContract.ADMIN_ROLE(), true);
+        superRbacContract.setRole(DEPLOYER, superRbacContract.HOOK_REGISTRATION_ROLE(), true);
+        superRbacContract.setRole(DEPLOYER, superRbacContract.HOOK_EXECUTOR_ROLE(), true);
+
+        superRbacContract.setRole(DEPLOYER, superRbacContract.SENTINELS_MANAGER(), true);
+        superRbacContract.setRole(DEPLOYER, superRbacContract.RELAYER_SENTINEL_MANAGER(), true);
+
+        SuperRegistry superRegistryContract = SuperRegistry(superRegistry);
+        superRegistryContract.setAddress(superRegistryContract.SUPER_RBAC_ID(), address(superRbacContract));
+        superRegistryContract.setAddress(superRegistryContract.RELAYER_ID(), RELAYER);
+        superRegistryContract.setAddress(superRegistryContract.RELAYER_SENTINEL_ID(), address(relayerSentinel));
         address deposit4626MintSuperPositionsDecoder;
         address deposit4626Module;
         // just for SuperTHAI
@@ -55,19 +69,6 @@ contract DeployV2Core is Script {
             ISentinel(relayerSentinel).addModuleToWhitelist(deposit4626Module);
             ISentinel(relayerSentinel).addDecoderToWhitelist(deposit4626MintSuperPositionsDecoder);
         }
-
-        SuperRbac superRbacContract = SuperRbac(superRbac);
-        superRbacContract.setRole(DEPLOYER, superRbacContract.ADMIN_ROLE(), true);
-        superRbacContract.setRole(DEPLOYER, superRbacContract.HOOK_REGISTRATION_ROLE(), true);
-        superRbacContract.setRole(DEPLOYER, superRbacContract.HOOK_EXECUTOR_ROLE(), true);
-
-        superRbacContract.setRole(DEPLOYER, superRbacContract.SENTINELS_MANAGER(), true);
-        superRbacContract.setRole(DEPLOYER, superRbacContract.RELAYER_SENTINEL_MANAGER(), true);
-
-        SuperRegistry superRegistryContract = SuperRegistry(superRegistry);
-        superRegistryContract.setAddress(superRegistryContract.SUPER_RBAC_ID(), address(superRbacContract));
-        superRegistryContract.setAddress(superRegistryContract.RELAYER_ID(), RELAYER);
-        superRegistryContract.setAddress(superRegistryContract.RELAYER_SENTINEL_ID(), address(relayerSentinel));
 
         // Log deployed addresses
         console2.log("SuperRBAC deployed to:", superRbac);
