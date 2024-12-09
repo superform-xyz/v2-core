d2d68b22f0ed9574b6609eb0de26fc9101f24aa8
Tamara Ringas <tamriasdev@gmail.com>
2024-12-09 18:12:29 +0200

    feat: update getEstimatedRewards()

--- contracts/src/libraries/strategies/StakedVaultSharesLibrary.sol
+++ contracts/src/libraries/strategies/StakedVaultSharesLibrary.sol
@@ -1,14 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.28;
 
+import { ISynthetixStakingRewards } from "@synthetixio/synthetix-staking-rewards/contracts/interfaces/ISynthetixStakingRewards.sol";
 import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 
 library SynthetixStakedVaultSharesLibrary {
   function getEstimatedRewards(
     address vault,
     address stakingProtocol,
-    uint256 amountToDeposit,
+    uint256 amountToDeposit
   ) internal view returns (uint256 rewards) {
     uint256 shares = IERC4626(vault).previewDeposit(amountToDeposit);
+    rewards = ISynthetixStakingRewards(stakingProtocol).getRewardForDuration(shares);
   }
 }
