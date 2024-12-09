7461f88a150dfd3942702b5d084337754ee3d5e7
Tamara Ringas <tamriasdev@gmail.com>
2024-12-09 18:08:40 +0200

    feat: add StakedVaultSharesLibrary

---
+++ contracts/src/libraries/strategies/StakedVaultSharesLibrary.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.28;
+
+import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+
+library SynthetixStakedVaultSharesLibrary {
+  function getEstimatedRewards(
+    address vault,
+    address stakingProtocol,
+    uint256 amountToDeposit,
+  ) internal view returns (uint256 rewards) {
+    uint256 shares = IERC4626(vault).previewDeposit(amountToDeposit);
+  }
+}
