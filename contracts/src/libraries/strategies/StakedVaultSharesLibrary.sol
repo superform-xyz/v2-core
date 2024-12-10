fcd3dc0d235bb3c412a88ec4454f3bd3497a51bb
Tamara Ringas <tamriasdev@gmail.com>
2024-12-10 18:50:37 +0200

    fix: update imports and contract name

--- contracts/src/libraries/strategies/StakedVaultSharesLibrary.sol
+++ contracts/src/libraries/strategies/StakedVaultSharesLibrary.sol
@@ -1,16 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.28;
 
-import { ISynthetixStakingRewards } from "@synthetixio/synthetix-staking-rewards/contracts/interfaces/ISynthetixStakingRewards.sol";
 import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 
-library SynthetixStakedVaultSharesLibrary {
+library CellarStakedVaultSharesLibrary {
   function getEstimatedRewards(
     address vault,
     address stakingProtocol,
     uint256 amountToDeposit
   ) internal view returns (uint256 rewards) {
     uint256 shares = IERC4626(vault).previewDeposit(amountToDeposit);
-    rewards = ISynthetixStakingRewards(stakingProtocol).getRewardForDuration(shares);
+    //rewards = IStakingRewards(stakingProtocol).getRewardForDuration(shares);
   }
 }
