1281f0a7d34c4bb3d312642c912c75853aeda5e4
Tamara Ringas <tamriasdev@gmail.com>
2024-12-11 18:42:14 +0200

    feat: add Deposit5115 Library

--- contracts/src/libraries/strategies/Deposit5115Library.sol
+++ contracts/src/libraries/strategies/Deposit5115Library.sol
@@ -1,8 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.28;
 
-import { IERC5115 } from "@openzeppelin/contracts/interfaces/IERC5115.sol";
+import { IStandardizedYield } from "../../interfaces/vendors/pendle/IStandardizedYield.sol";
 
 library Deposit5115Library {
-
+  function getEstimatedRewards(
+    address standardizedYield,
+    address tokenIn,
+    uint256 amountToDeposit
+  ) external view returns (uint256 reward) {
+    reward = IStandardizedYield(standardizedYield).previewDeposit(tokenIn, amountToDeposit);
+  }
 }
