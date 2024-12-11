5db8942108e068d103533800294a6a4ebc9e580e
Tamara Ringas <tamriasdev@gmail.com>
2024-12-11 18:41:53 +0200

    feat: add Mock5115Vault contract

---
+++ contracts/test/mocks/Mock5115Vault.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.28;
+
+import { ERC20Mock } from "./ERC20Mock.sol";
+import { ERC20 } from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import { IERC20 } from "openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+import { SafeERC20 } from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract Mock5115Vault {
+  constructor(
+    IERC20 asset_,
+    string memory name_,
+    string memory symbol_
+  ) {}
+
+  function previewDeposit(address tokenIn, uint256 amountTokenToDeposit) external view returns (uint256 amountSharesOut) {
+    amountSharesOut = amountTokenToDeposit;
+  }
+}
+
