254cee9b4371f5f5fcc7fb1aabcc0c83c93f0356
Tamara Ringas <tamriasdev@gmail.com>
2024-12-14 13:15:51 +0200

    fix: add library wrapper

--- contracts/test/unit/libraries/Looped4626DepositLibrary.t.sol
+++ contracts/test/unit/libraries/Looped4626DepositLibrary.t.sol
@@ -21,19 +21,57 @@ contract Looped4626DepositLibraryTest is BaseTest {
     super.setUp();
     asset = new MockERC20("Asset", "ASSET", 18);
     asset2 = new MockERC20("Asset2", "ASSET2", 18);
-    vault = new Mock4626Vault(address(asset));
-    vault2 = new Mock4626Vault(address(asset));
+    vault = new Mock4626Vault(
+      IERC20(address(asset)),
+      "Vault",
+      "V"
+    );
+    vault2 = new Mock4626Vault(
+      IERC20(address(asset)),
+      "Vault2",
+      "V2"
+    );
     wrapper = new Looped4626DepositLibraryWrapper();
   }
 
-  function test_getEstimatedRewardsSingleVault() public view {
+  function test_getPricePerShareSingleVault() public view {
     uint256 loops = 10;
-    uint256 amountPerLoop = 100;
-    uint256 rewards = Looped4626DepositLibrary.getEstimatedRewards(
+    uint256 rewards = Looped4626DepositLibrary.getPricePerShare(
       address(vault),
-      loops,
-      amountPerLoop
+      loops
     );
-    assertEq(rewards, 1000);
+    assertEq(rewards, 10e18);
+  }
+
+  function test_getPricePerShareMultiVault() public view {
+    address[] memory vaults = new address[](2);
+    vaults[0] = address(vault);
+    vaults[1] = address(vault2);
+    uint256[] memory rewards = wrapper.getPricePerShareMultiVault(vaults, address(asset), 10);
+    assertEq(rewards[0], 10e18);
+    assertEq(rewards[1], 10e18);
+  }
+
+  function test_getPricePerShareMultiVault_differentUnderlyingAsset() public {
+    address[] memory vaults = new address[](2);
+    vaults[0] = address(vault);
+    Mock4626Vault vault3 = new Mock4626Vault(
+      IERC20(address(asset2)),
+      "Vault3",
+      "V3"
+    );
+    vaults[1] = address(vault3);
+    vm.expectRevert(Looped4626DepositLibrary.VAULTS_MUST_HAVE_SAME_UNDERLYING_ASSET.selector);
+    wrapper.getPricePerShareMultiVault(vaults, address(asset2), 10);
+  }
+}
+
+contract Looped4626DepositLibraryWrapper {
+  function getPricePerShare(address vault, uint256 loops) external view returns (uint256) {
+    return Looped4626DepositLibrary.getPricePerShare(vault, loops);
+  }
+
+  function getPricePerShareMultiVault(address[] memory vaults, address underlyingAsset, uint256 loops) external view returns (uint256[] memory) {
+    return Looped4626DepositLibrary.getPricePerShareMultiVault(vaults, underlyingAsset, loops);
   }
 }
