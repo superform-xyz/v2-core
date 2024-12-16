c1ac924b22da7d4205534418228f69d666ec8060
Tamara Ringas <tamriasdev@gmail.com>
2024-12-16 14:28:31 +0200

    fix: update test_get5115StrategyPrices()

--- contracts/test/unit/strategies/oracles/Deposit5115ActionOracle.t.sol
+++ contracts/test/unit/strategies/oracles/Deposit5115ActionOracle.t.sol
@@ -20,19 +20,19 @@ contract Deposit5115ActionOracleTest is BaseTest {
     vault = new Mock5115Vault(IERC20(address(underlying)), "Vault", "VAULT");
   }
 
-  function test_getStrategyPrice() public view {
+  function test_get5115StrategyPrice() public view {
     uint256 pricePerShare = oracle.getStrategyPrice(address(vault), address(underlying));
     assertEq(pricePerShare, 1e18);
   }
 
-  function test_getStrategyPrices() public {
+  function test_get5115StrategyPrices() public {
     address[] memory finalTargets = new address[](1);
     finalTargets[0] = address(vault);
 
     address[] memory assets = new address[](1);
     assets[0] = address(underlying);
 
-    uint256[] memory prices = oracle.getStrategyPrices(finalTargets, assets);
+    uint256[] memory prices = oracle.getStrategyPrices(assets, finalTargets);
     assertEq(prices[0], 1e18);
   }
 }
