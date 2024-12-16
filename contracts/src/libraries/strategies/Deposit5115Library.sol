e9d73841dc4017d929668e5ce26ee57fb82fa19c
Tamara Ringas <tamriasdev@gmail.com>
2024-12-16 15:02:06 +0200

    chore: format

--- contracts/src/libraries/strategies/Deposit5115Library.sol
+++ contracts/src/libraries/strategies/Deposit5115Library.sol
@@ -4,18 +4,12 @@ pragma solidity >=0.8.28;
 import { IStandardizedYield } from "../../interfaces/vendors/pendle/IStandardizedYield.sol";
 
 library Deposit5115Library {
-  /// @notice Get the price per share for a deposit into a 5115 vault
-  /// @param finalTarget The address of the final target vault
-  /// @param tokenIn The address of the token to receive after redeeming
-  /// @return pricePerShare The price per share
-  function getPricePerShare(
-    address finalTarget,
-    address tokenIn
-  ) internal view returns (uint256 pricePerShare) {
-    (,, uint256 decimals) = IStandardizedYield(finalTarget).assetInfo();
-    pricePerShare = IStandardizedYield(finalTarget).previewRedeem(
-      tokenIn,
-      10 ** decimals
-    );
-  }
+    /// @notice Get the price per share for a deposit into a 5115 vault
+    /// @param finalTarget The address of the final target vault
+    /// @param tokenIn The address of the token to receive after redeeming
+    /// @return pricePerShare The price per share
+    function getPricePerShare(address finalTarget, address tokenIn) internal view returns (uint256 pricePerShare) {
+        (,, uint256 decimals) = IStandardizedYield(finalTarget).assetInfo();
+        pricePerShare = IStandardizedYield(finalTarget).previewRedeem(tokenIn, 10 ** decimals);
+    }
 }
