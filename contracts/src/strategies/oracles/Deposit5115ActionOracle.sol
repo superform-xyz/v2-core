9918f98bd2ab938da077f84f554aeb53ecf1dc59
Tamara Ringas <tamriasdev@gmail.com>
2024-12-16 11:48:13 +0200

    feat: add view methods

--- contracts/src/strategies/oracles/Deposit5115ActionOracle.sol
+++ contracts/src/strategies/oracles/Deposit5115ActionOracle.sol
@@ -8,5 +8,46 @@ import { Deposit5115Library } from "../../libraries/strategies/Deposit5115Librar
 /// @author Superform Labs
 /// @notice Oracle for the Deposit Action in 5115 Vaults
 contract Deposit5115ActionOracle is IActionOracle {
+  /*//////////////////////////////////////////////////////////////
+                            CONSTRUCTOR
+  //////////////////////////////////////////////////////////////*/
+
   constructor() {}
+
+  /*//////////////////////////////////////////////////////////////
+                           VIEW METHODS
+  //////////////////////////////////////////////////////////////*/
+
+  /// @inheritdoc IActionOracle
+  function getStrategyPrice(
+    address finalTarget
+  ) public view returns (uint256 price) {
+    price = Deposit5115Library.getPricePerShare(finalTarget);
+  }
+
+  /// @inheritdoc IActionOracle
+  function getStrategyPrices(
+    address[] memory finalTargets
+  ) external view returns (uint256[] memory prices) {
+    prices = new uint256[](finalTargets.length);
+    for (uint256 i = 0; i < finalTargets.length; i++) {
+      prices[i] = getStrategyPrice(finalTargets[i]);
+    }
+  }
+
+  // ToDo: Implement this with the metadata library
+  /// @inheritdoc IActionOracle
+  function getVaultStrategyMetadata(
+    address finalTarget
+  ) external view returns (bytes memory metadata) {
+    return "0x0";
+  }
+
+  // ToDo: Implement this with the metadata library
+  /// @inheritdoc IActionOracle
+  function getVaultsStrategyMetadata(
+    address[] memory finalTargets
+  ) external view returns (bytes[] memory metadata) {
+    return new bytes[](finalTargets.length);
+  }
 }
