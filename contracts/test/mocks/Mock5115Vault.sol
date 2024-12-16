9d7c521b0687a7f6128305b692b55125c81fc442
Tamara Ringas <tamriasdev@gmail.com>
2024-12-16 13:13:44 +0200

    feat: add assetInfo() function

--- contracts/test/mocks/Mock5115Vault.sol
+++ contracts/test/mocks/Mock5115Vault.sol
@@ -21,14 +21,27 @@ contract Mock5115Vault {
     asset = new MockERC20(name_, symbol_, 18);
   }
 
-    function previewDeposit(
-        address tokenIn,
-        uint256 amountTokenToDeposit
-    )
-        external
-        view
-        returns (uint256 amountSharesOut)
-    {
-        amountSharesOut = amountTokenToDeposit;
-    }
+  function assetInfo() external view returns (
+    AssetType assetType,
+    address asset_,
+    uint8 decimals
+  ) {
+    assetType = AssetType.ERC20;
+    asset_ = address(asset);
+    decimals = uint8(asset.decimals());
+  }
+
+  function previewDeposit(
+    address tokenIn,
+    uint256 amountTokenToDeposit
+  ) external view returns (uint256 amountSharesOut) {
+    amountSharesOut = amountTokenToDeposit;
+  }
+
+  function previewRedeem(
+    address tokenIn,
+    uint256 amountSharesToRedeem
+  ) external view returns (uint256 amountTokenOut) {
+    amountTokenOut = amountSharesToRedeem;
+  }
 }
