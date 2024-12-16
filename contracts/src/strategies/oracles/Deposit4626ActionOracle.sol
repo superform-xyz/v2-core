ddb69b40bb4804d0c83f55ab07788bdb59a0d10f
Tamara Ringas <tamriasdev@gmail.com>
2024-12-16 11:46:50 +0200

    fix: rm unused import statement

--- contracts/src/strategies/oracles/Deposit4626ActionOracle.sol
+++ contracts/src/strategies/oracles/Deposit4626ActionOracle.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.28;
 
-import { SuperRbac } from "../../settings/SuperRbac.sol";
 import { IActionOracle } from "../../interfaces/strategies/IActionOracle.sol";
 import { Deposit4626Library } from "../../libraries/strategies/Deposit4626Library.sol";
 
