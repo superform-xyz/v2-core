647bc47be7819f6b0391a5a0792e7891c502d5a2
Tamara Ringas <tamriasdev@gmail.com>
2024-12-24 11:20:33 +0200

    feat: instantiate contract

---
+++ contracts/test/integration/SameChainDepositRedeemFlow.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.28;
+
+import { ForkedTestBase } from "./ForkedTestBase.t.sol";
+
+contract SameChainDepositRedeemFlowTest is ForkedTestBase {
+
+}
