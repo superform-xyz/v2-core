84912f5a143084d98a0142bf0090e5b016560183
Tamara Ringas <tamriasdev@gmail.com>
2024-12-30 11:29:26 +0200

    fix: update hooksData

--- contracts/test/integration/SameChainDepositRedeemFlow.t.sol
+++ contracts/test/integration/SameChainDepositRedeemFlow.t.sol
@@ -38,28 +38,28 @@ contract SameChainDepositRedeemFlowTest is ForkedTestBase {
         bytes[] memory redeemHooksData = _createWithdrawActionData(finalTarget, amount);
 
         // extra non-action set of hooks
-        bytes[] memory nonMainActionHooksData = new bytes[](1);
-        nonMainActionHooksData[0] = abi.encode(underlying, instance.account, amount); //Should account be user2?
+        //bytes[] memory nonMainActionHooksData = new bytes[](1);
+        //nonMainActionHooksData[0] = abi.encode(underlying, instance.account, amount); //Should account be user2?
         //nonMainActionHooksData[1] = abi.encode(finalTarget, instance.account, amount);
 
-        address[] memory nonMainActionHooks = new address[](1);
-        nonMainActionHooks[0] = address(approveErc20Hook);
+        //address[] memory nonMainActionHooks = new address[](1);
+        //nonMainActionHooks[0] = _getHook(chainIds[0], "ApproveERC20Hook");
         //nonMainActionHooks[1] = address(deposit4626VaultHook);
 
         // it should execute all hooks
-        ISuperExecutorV2.ExecutorEntry[] memory entries = new ISuperExecutorV2.ExecutorEntry[](2);
+        ISuperExecutorV2.ExecutorEntry[] memory entries = new ISuperExecutorV2.ExecutorEntry[](1);
         entries[0] = ISuperExecutorV2.ExecutorEntry({
             actionId: ACTION["4626_DEPOSIT"],
             finalTarget: finalTarget,
             hooksData: depositHooksData,
             nonMainActionHooks: new address[](0)
         });
-        entries[1] = ISuperExecutorV2.ExecutorEntry({
-            actionId: type(uint256).max,
-            finalTarget: address(0),
-            hooksData: nonMainActionHooksData,
-            nonMainActionHooks: nonMainActionHooks
-        });
+        // entries[1] = ISuperExecutorV2.ExecutorEntry({
+        //     actionId: type(uint256).max,
+        //     finalTarget: address(0),
+        //     hooksData: nonMainActionHooksData,
+        //     nonMainActionHooks: nonMainActionHooks
+        // });
         
         //  entries[1] = ISuperExecutorV2.ExecutorEntry({
         //     actionId: ACTION["4626_WITHDRAW"],
@@ -73,7 +73,7 @@ contract SameChainDepositRedeemFlowTest is ForkedTestBase {
         // emit ISuperActions.AccountingUpdated(
         //     instance.account, ACTION["4626_DEPOSIT"], finalTarget, true, amount, 1e18
         // );
-        superExecutor = ISuperExecutorV2(superExecutorsAddresses[chainIds[0]]);
+        superExecutor = ISuperExecutorV2(_getContract(chainIds[0], "SuperExecutorV2"));
         superExecutor.execute(instance.account, abi.encode(entries));
 
         uint256 accSharesAfter = vaultInstance.balanceOf(instance.account);
