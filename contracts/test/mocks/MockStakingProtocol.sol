ed642335b2638e90b61b1c17c046670dfb0d2e4e
Tamara Ringas <tamriasdev@gmail.com>
2025-01-09 17:54:00 +0200

    fix: update rewardPerToken to return timestamp

--- contracts/test/mocks/MockStakingProtocol.sol
+++ contracts/test/mocks/MockStakingProtocol.sol
@@ -2,10 +2,15 @@
 pragma solidity >=0.8.28;
 
 contract MockStakingProtocol {
-    function rewardPerToken() external pure returns (uint256) {
-        return 1e18;
+    /// @notice Mock the rewardPerToken function
+    /// @return rewardPerToken The reward per token
+    /// @return latestTimestamp The latest timestamp
+    function rewardPerToken() external view returns (uint256, uint256) {
+        return (1e18, block.timestamp);
     }
 
+    /// @notice Mock the activeRewardAmount function
+    /// @return activeRewardAmount The active reward amount
     function activeRewardAmount() external pure returns (uint256) {
         return 1e18;
     }
