54294758955ed6466a331928a7eb4d467a2d5027
Tamara Ringas <tamriasdev@gmail.com>
2025-01-09 17:22:03 +0200

    fix: update function visibility

--- contracts/test/mocks/MockStakingProtocol.sol
+++ contracts/test/mocks/MockStakingProtocol.sol
@@ -2,11 +2,11 @@
 pragma solidity >=0.8.28;
 
 contract MockStakingProtocol {
-    function rewardPerToken() external view returns (uint256) {
+    function rewardPerToken() external pure returns (uint256) {
         return 1e18;
     }
 
-    function activeRewardAmount() external view returns (uint256) {
+    function activeRewardAmount() external pure returns (uint256) {
         return 1e18;
     }
 }
