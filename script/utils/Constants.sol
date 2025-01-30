4b9926d01844bb0335af847fb8924a02a2d3750e
supervaulter <cosmin@superform.xyz>
2025-01-30 16:05:27 +0200

    chore: minor constants.sol fix

--- script/utils/Constants.sol
+++ script/utils/Constants.sol
@@ -2,41 +2,41 @@
 pragma solidity >=0.8.28;
 
 abstract contract Constants {
-    address public constant ENTRY_POINT = 0x0000000071727De22E5E9d8BAf0edAc6f37da032;
+    address internal constant ENTRY_POINT = 0x0000000071727De22E5E9d8BAf0edAc6f37da032;
     
     // chain names
-    string public constant ETHEREUM_KEY = "Ethereum";
-    string public constant BASE_KEY = "Base";
-    string public constant OPTIMISM_KEY = "Optimism";
-    string public constant ARBITRUM_KEY = "Arbitrum";
-    string public constant SEPOLIA_KEY = "Sepolia";
-    string public constant ARB_SEPOLIA_KEY = "Arbitrum_Sepolia";
-    string public constant BASE_SEPOLIA_KEY = "Base_Sepolia";
-    string public constant OP_SEPOLIA_KEY = "OP_Sepolia";
-    string public constant POLYGON_KEY = "Polygon";
+    string internal constant ETHEREUM_KEY = "Ethereum";
+    string internal constant BASE_KEY = "Base";
+    string internal constant OPTIMISM_KEY = "Optimism";
+    string internal constant ARBITRUM_KEY = "Arbitrum";
+    string internal constant SEPOLIA_KEY = "Sepolia";
+    string internal constant ARB_SEPOLIA_KEY = "Arbitrum_Sepolia";
+    string internal constant BASE_SEPOLIA_KEY = "Base_Sepolia";
+    string internal constant OP_SEPOLIA_KEY = "OP_Sepolia";
+    string internal constant POLYGON_KEY = "Polygon";
 
     // keys
-    string public constant SUPER_REGISTRY_KEY = "SuperRegistry";
-    string public constant SUPER_EXECUTOR_KEY = "SuperExecutor";
-    string public constant SUPER_RBAC_KEY = "SuperRbac";
-    string public constant SUPER_LEDGER_KEY = "SuperLedger";
-    string public constant SUPER_POSITION_KEY = "SuperPosition";
-    string public constant SUPER_POSITION_SENTINEL_KEY = "SuperPositionSentinel";
-    string public constant ACROSS_RECEIVE_FUNDS_AND_EXECUTE_GATEWAY_KEY = "AcrossReceiveFundsAndExecuteGateway";
-    string public constant DEBRIDGE_RECEIVE_FUNDS_AND_EXECUTE_GATEWAY_KEY = "DeBridgeReceiveFundsAndExecuteGateway";
-    string public constant MOCK_VALIDATOR_MODULE_KEY = "MockValidatorModule";
+    string internal constant SUPER_REGISTRY_KEY = "SuperRegistry";
+    string internal constant SUPER_EXECUTOR_KEY = "SuperExecutor";
+    string internal constant SUPER_RBAC_KEY = "SuperRbac";
+    string internal constant SUPER_LEDGER_KEY = "SuperLedger";
+    string internal constant SUPER_POSITION_KEY = "SuperPosition";
+    string internal constant SUPER_POSITION_SENTINEL_KEY = "SuperPositionSentinel";
+    string internal constant ACROSS_RECEIVE_FUNDS_AND_EXECUTE_GATEWAY_KEY = "AcrossReceiveFundsAndExecuteGateway";
+    string internal constant DEBRIDGE_RECEIVE_FUNDS_AND_EXECUTE_GATEWAY_KEY = "DeBridgeReceiveFundsAndExecuteGateway";
+    string internal constant MOCK_VALIDATOR_MODULE_KEY = "MockValidatorModule";
 
     // mainnets
-    uint64 public constant MAINNET_CHAIN_ID = 1;
-    uint64 public constant BASE_CHAIN_ID = 8453;
-    uint64 public constant OPTIMISM_CHAIN_ID = 10;
-    uint64 public constant POLYGON_CHAIN_ID = 137;
-    uint64 public constant ARBITRUM_CHAIN_ID = 42_161;
+    uint64 internal constant MAINNET_CHAIN_ID = 1;
+    uint64 internal constant BASE_CHAIN_ID = 8453;
+    uint64 internal constant OPTIMISM_CHAIN_ID = 10;
+    uint64 internal constant POLYGON_CHAIN_ID = 137;
+    uint64 internal constant ARBITRUM_CHAIN_ID = 42_161;
     // testnets
-    uint64 public constant SEPOLIA_CHAIN_ID = 11_155_111;
-    uint64 public constant ARB_SEPOLIA_CHAIN_ID = 421_613;
-    uint64 public constant BASE_SEPOLIA_CHAIN_ID = 84_532;
-    uint64 public constant OP_SEPOLIA_CHAIN_ID = 11_155_420;
+    uint64 internal constant SEPOLIA_CHAIN_ID = 11_155_111;
+    uint64 internal constant ARB_SEPOLIA_CHAIN_ID = 421_613;
+    uint64 internal constant BASE_SEPOLIA_CHAIN_ID = 84_532;
+    uint64 internal constant OP_SEPOLIA_CHAIN_ID = 11_155_420;
 
     // Common addresses
     address internal constant SUPER_DEPLOYER = 0x4b38341B1126F45614B26319787CA98aeC1b6f57;
@@ -60,34 +60,34 @@ abstract contract Constants {
     address internal constant DEBRIDGE_GATE_OP_SEPOLIA = address(0);
 
     // Hook Keys
-    string constant ACROSS_SEND_FUNDS_AND_EXECUTE_ON_DST_HOOK_KEY = "AcrossSendFundsAndExecuteOnDstHook";
-    string constant DEBRIDGE_SEND_FUNDS_AND_EXECUTE_ON_DST_HOOK_KEY = "DeBridgeSendFundsAndExecuteOnDstHook";
-    string constant FLUID_CLAIM_REWARD_HOOK_KEY = "FluidClaimRewardHook";
-    string constant GEARBOX_CLAIM_REWARD_HOOK_KEY = "GearboxClaimRewardHook";
-    string constant SOMELIER_CLAIM_ALL_REWARDS_HOOK_KEY = "SomelierClaimAllRewardsHook";
-    string constant SOMELIER_CLAIM_ONE_REWARD_HOOK_KEY = "SomelierClaimOneRewardHook";
-    string constant YEARN_CLAIM_ALL_REWARDS_HOOK_KEY = "YearnClaimAllRewardsHook";
-    string constant YEARN_CLAIM_ONE_REWARD_HOOK_KEY = "YearnClaimOneRewardHook";
-    string constant APPROVE_ERC20_HOOK_KEY = "ApproveERC20Hook";
-    string constant TRANSFER_ERC20_HOOK_KEY = "TransferERC20Hook";
-    string constant DEPOSIT_4626_VAULT_HOOK_KEY = "Deposit4626VaultHook";
-    string constant WITHDRAW_4626_VAULT_HOOK_KEY = "Withdraw4626VaultHook";
-    string constant DEPOSIT_5115_VAULT_HOOK_KEY = "Deposit5115VaultHook";
-    string constant WITHDRAW_5115_VAULT_HOOK_KEY = "Withdraw5115VaultHook";
-    string constant REQUEST_DEPOSIT_7540_VAULT_HOOK_KEY = "RequestDeposit7540VaultHook";
-    string constant REQUEST_WITHDRAW_7540_VAULT_HOOK_KEY = "RequestWithdraw7540VaultHook";
-    string constant GEARBOX_STAKE_HOOK_KEY = "GearboxStakeHook";
-    string constant GEARBOX_WITHDRAW_HOOK_KEY = "GearboxWithdrawHook";
-    string constant SOMELIER_STAKE_HOOK_KEY = "SomelierStakeHook";
-    string constant SOMELIER_UNBOND_ALL_HOOK_KEY = "SomelierUnbondAllHook";
-    string constant SOMELIER_UNBOND_HOOK_KEY = "SomelierUnbondHook";
-    string constant SOMELIER_UNSTAKE_ALL_HOOK_KEY = "SomelierUnstakeAllHook";
-    string constant SOMELIER_UNSTAKE_HOOK_KEY = "SomelierUnstakeHook";
-    string constant YEARN_WITHDRAW_HOOK_KEY = "YearnWithdrawHook";
-    string constant YIELD_EXIT_HOOK_KEY = "YieldExitHook";
+    string internal constant ACROSS_SEND_FUNDS_AND_EXECUTE_ON_DST_HOOK_KEY = "AcrossSendFundsAndExecuteOnDstHook";
+    string internal constant DEBRIDGE_SEND_FUNDS_AND_EXECUTE_ON_DST_HOOK_KEY = "DeBridgeSendFundsAndExecuteOnDstHook";
+    string internal constant FLUID_CLAIM_REWARD_HOOK_KEY = "FluidClaimRewardHook";
+    string internal constant GEARBOX_CLAIM_REWARD_HOOK_KEY = "GearboxClaimRewardHook";
+    string internal constant SOMELIER_CLAIM_ALL_REWARDS_HOOK_KEY = "SomelierClaimAllRewardsHook";
+    string internal constant SOMELIER_CLAIM_ONE_REWARD_HOOK_KEY = "SomelierClaimOneRewardHook";
+    string internal constant YEARN_CLAIM_ALL_REWARDS_HOOK_KEY = "YearnClaimAllRewardsHook";
+    string internal constant YEARN_CLAIM_ONE_REWARD_HOOK_KEY = "YearnClaimOneRewardHook";
+    string internal constant APPROVE_ERC20_HOOK_KEY = "ApproveERC20Hook";
+    string internal constant TRANSFER_ERC20_HOOK_KEY = "TransferERC20Hook";
+    string internal constant DEPOSIT_4626_VAULT_HOOK_KEY = "Deposit4626VaultHook";
+    string internal constant WITHDRAW_4626_VAULT_HOOK_KEY = "Withdraw4626VaultHook";
+    string internal constant DEPOSIT_5115_VAULT_HOOK_KEY = "Deposit5115VaultHook";
+    string internal constant WITHDRAW_5115_VAULT_HOOK_KEY = "Withdraw5115VaultHook";
+    string internal constant REQUEST_DEPOSIT_7540_VAULT_HOOK_KEY = "RequestDeposit7540VaultHook";
+    string internal constant REQUEST_WITHDRAW_7540_VAULT_HOOK_KEY = "RequestWithdraw7540VaultHook";
+    string internal constant GEARBOX_STAKE_HOOK_KEY = "GearboxStakeHook";
+    string internal constant GEARBOX_WITHDRAW_HOOK_KEY = "GearboxWithdrawHook";
+    string internal constant SOMELIER_STAKE_HOOK_KEY = "SomelierStakeHook";
+    string internal constant SOMELIER_UNBOND_ALL_HOOK_KEY = "SomelierUnbondAllHook";
+    string internal constant SOMELIER_UNBOND_HOOK_KEY = "SomelierUnbondHook";
+    string internal constant SOMELIER_UNSTAKE_ALL_HOOK_KEY = "SomelierUnstakeAllHook";
+    string internal constant SOMELIER_UNSTAKE_HOOK_KEY = "SomelierUnstakeHook";
+    string internal constant YEARN_WITHDRAW_HOOK_KEY = "YearnWithdrawHook";
+    string internal constant YIELD_EXIT_HOOK_KEY = "YieldExitHook";
 
     // oracle keys
-    string constant ERC4626_YIELD_SOURCE_ORACLE_KEY = "ERC4626YieldSourceOracle";
-    string constant ERC5115_YIELD_SOURCE_ORACLE_KEY = "ERC5115YieldSourceOracle";
+    string internal constant ERC4626_YIELD_SOURCE_ORACLE_KEY = "ERC4626YieldSourceOracle";
+    string internal constant ERC5115_YIELD_SOURCE_ORACLE_KEY = "ERC5115YieldSourceOracle";
 }
 
