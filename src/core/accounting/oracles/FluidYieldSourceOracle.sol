f625914629ddecf79678432fe516f397b7566e98
0xTimepunk <jldcmartins@gmail.com>
2025-02-03 19:01:08 +0000

    fix: fixes fluid, adds getTVLMultiple, removes warnings

--- src/core/accounting/oracles/FluidYieldSourceOracle.sol
+++ src/core/accounting/oracles/FluidYieldSourceOracle.sol
@@ -2,28 +2,76 @@
 pragma solidity >=0.8.28;
 
 import { IYieldSourceOracle } from "../../interfaces/accounting/IYieldSourceOracle.sol";
-import { FluidYieldSourceOracleLibrary } from "../../libraries/accounting/FluidYieldSourceOracleLibrary.sol";
+import { IFluidLendingStakingRewards } from "../../interfaces/vendors/fluid/IFluidLendingStakingRewards.sol";
 
 /// @title FluidYieldSourceOracle
 /// @author Superform Labs
 /// @notice Oracle for Fluid yield source
 contract FluidYieldSourceOracle is IYieldSourceOracle {
     /// @inheritdoc IYieldSourceOracle
-    function getTVL(address yieldSourceAddress, address ownerOfShares) public view returns (uint256 tvl) {
-        revert("Not implemented");
-    }
-
-    /// @inheritdoc IYieldSourceOracle
-    function getPricePerShare(address yieldSourceAddress) public view returns (uint256 price) {
-        price = FluidYieldSourceOracleLibrary.getPricePerShare(yieldSourceAddress);
+    function getPricePerShare(address yieldSourceAddress) public view returns (uint256 pricePerShare) {
+        pricePerShare = IFluidLendingStakingRewards(yieldSourceAddress).rewardPerToken();
     }
 
     /// @inheritdoc IYieldSourceOracle
     function getPricePerShareMultiple(address[] memory yieldSourceAddresses)
         external
         view
-        returns (uint256[] memory prices)
+        returns (uint256[] memory pricesPerShare)
     {
-        prices = FluidYieldSourceOracleLibrary.getPricePerShareMultiple(yieldSourceAddresses);
+        uint256 length = yieldSourceAddresses.length;
+        pricesPerShare = new uint256[](length);
+        for (uint256 i = 0; i < length;) {
+            address yieldAddress = yieldSourceAddresses[i];
+            pricesPerShare[i] = IFluidLendingStakingRewards(yieldAddress).rewardPerToken();
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    /// @inheritdoc IYieldSourceOracle
+    function getTVL(address yieldSourceAddress, address ownerOfShares) public view returns (uint256 tvl) {
+        tvl = IFluidLendingStakingRewards(yieldSourceAddress).balanceOf(ownerOfShares)
+            * IFluidLendingStakingRewards(yieldSourceAddress).rewardPerToken();
+    }
+
+    /// @inheritdoc IYieldSourceOracle
+    function getTVLMultiple(
+        address[] memory yieldSourceAddresses,
+        address[][] memory ownersOfShares
+    )
+        external
+        view
+        returns (uint256[][] memory userTvls, uint256[] memory totalTvls)
+    {
+        uint256 length = yieldSourceAddresses.length;
+        if (length != ownersOfShares.length) revert ARRAY_LENGTH_MISMATCH();
+
+        userTvls = new uint256[][](length);
+        totalTvls = new uint256[](length);
+
+        for (uint256 i = 0; i < length;) {
+            address yieldSource = yieldSourceAddresses[i];
+            address[] memory owners = ownersOfShares[i];
+            uint256 ownersLength = owners.length;
+
+            userTvls[i] = new uint256[](ownersLength);
+            uint256 totalTvl = 0;
+
+            for (uint256 j = 0; j < ownersLength;) {
+                uint256 userTvl = getTVL(yieldSource, owners[j]);
+                userTvls[i][j] = userTvl;
+                totalTvl += userTvl;
+                unchecked {
+                    ++j;
+                }
+            }
+
+            totalTvls[i] = totalTvl;
+            unchecked {
+                ++i;
+            }
+        }
     }
 }
--- src/core/interfaces/accounting/IYieldSourceOracle.sol
+++ src/core/interfaces/accounting/IYieldSourceOracle.sol
@@ -6,8 +6,28 @@ pragma solidity >=0.8.28;
 /// @notice Interface for Yield Source Oracles
 interface IYieldSourceOracle {
     /*//////////////////////////////////////////////////////////////
+                                ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Error thrown when array lengths do not match in batch operations
+    error ARRAY_LENGTH_MISMATCH();
+
+    /*//////////////////////////////////////////////////////////////
                             VIEW METHODS
     //////////////////////////////////////////////////////////////*/
+
+    /// @notice Derives the price of an action
+    /// @param yieldSourceAddress The yield source to derive the price for
+    /// @return pricePerShare The price of the action
+    function getPricePerShare(address yieldSourceAddress) external view returns (uint256 pricePerShare);
+
+    /// @notice Derives the price of an action for multiple yield sources
+    /// @param yieldSourceAddresses Array of yield sources to derive the price for
+    /// @return pricesPerShare Array of prices for each yield source
+    function getPricePerShareMultiple(address[] memory yieldSourceAddresses)
+        external
+        view
+        returns (uint256[] memory pricesPerShare);
 
     /// @notice Derives the TVL in a yield source by a given owner of shares
     /// @param yieldSourceAddress The yield source to derive TVL for
@@ -15,16 +35,16 @@ interface IYieldSourceOracle {
     /// @return tvl The TVL of the yield source by the owner of the shares
     function getTVL(address yieldSourceAddress, address ownerOfShares) external view returns (uint256 tvl);
 
-    /// @notice Derives the price of an action
-    /// @param yieldSourceAddress The yield source to derive the price for
-    /// @return price The price of the action
-    function getPricePerShare(address yieldSourceAddress) external view returns (uint256 price);
-
-    /// @notice Gets the price per share for multiple yield sources
-    /// @param yieldSourceAddresses The yield sources to derive the price for
-    /// @return prices The prices of the yield sources
-    function getPricePerShareMultiple(address[] memory yieldSourceAddresses)
+    /// @notice Derives the TVL in multiple yield sources for multiple owners
+    /// @param yieldSourceAddresses Array of yield sources to derive TVL for
+    /// @param ownersOfShares Array of arrays containing owner addresses for each yield source
+    /// @return userTvls Array of arrays containing TVLs for each owner in each yield source
+    /// @return totalTvls Array containing total TVL for each yield source
+    function getTVLMultiple(
+        address[] memory yieldSourceAddresses,
+        address[][] memory ownersOfShares
+    )
         external
         view
-        returns (uint256[] memory prices);
+        returns (uint256[][] memory userTvls, uint256[] memory totalTvls);
 }
