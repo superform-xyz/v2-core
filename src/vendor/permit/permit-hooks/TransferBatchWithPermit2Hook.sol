0e81a502632b5694627450f4fb01f260a05921d3
Tamara Ringas <tamriasdev@gmail.com>
2025-02-18 13:53:44 +0200

    feat: move permit hooks to separate directory and update imports

--- src/vendor/permit/permit-hooks/TransferBatchWithPermit2Hook.sol
+++ src/vendor/permit/permit-hooks/TransferBatchWithPermit2Hook.sol
@@ -2,18 +2,18 @@
 pragma solidity >=0.8.28;
 
 // external
-import { BytesLib } from "../../../../vendor/BytesLib.sol";
+import { BytesLib } from "../../../vendor/BytesLib.sol";
 import { Execution } from "modulekit/accounts/erc7579/lib/ExecutionLib.sol";
 import { SafeCast } from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
 import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
 
 // Superform
-import { BaseHook } from "../../BaseHook.sol";
+import { BaseHook } from "../../../../src/core/hooks/BaseHook.sol";
 
-import { ISuperHook, ISuperHookResult } from "../../../interfaces/ISuperHook.sol";
-import { IPermit2Batch } from "../../../../vendor/uniswap/permit2/IPermit2Batch.sol";
-import { IAllowanceTransfer } from "../../../../vendor/uniswap/permit2/IAllowanceTransfer.sol";
+import { ISuperHook, ISuperHookResult } from "../../../../src/core/interfaces/ISuperHook.sol";
+import { IPermit2Batch } from "../../../vendor/uniswap/permit2/IPermit2Batch.sol";
+import { IAllowanceTransfer } from "../../../vendor/uniswap/permit2/IAllowanceTransfer.sol";
 
 /// @title TransferBatchWithPermit2Hook
 /// @dev data has the following structure
