da8094e5910433bde109cbf79d24ea766845105c
supervaulter <cosmin@superform.xyz>
2025-02-19 12:42:00 +0200

    feat: hook registry

---
+++ src/core/hooks/HooksRegistry.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.28;
+
+// Superform
+import { SuperRegistryImplementer } from "../utils/SuperRegistryImplementer.sol";
+
+import { ISuperRbac } from "../interfaces/ISuperRbac.sol";
+
+contract HooksRegistry is SuperRegistryImplementer {
+    /*//////////////////////////////////////////////////////////////
+                                 STORAGE
+    //////////////////////////////////////////////////////////////*/
+    mapping(address => bool) public isHookRegistered;
+    address[] public registeredHooks;
+
+    constructor(address registry_) SuperRegistryImplementer(registry_) {}
+
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
+    error NOT_AUTHORIZED();
+    error HOOK_NOT_REGISTERED();
+    error HOOK_ALREADY_REGISTERED();
+
+    modifier onlyHooksManager() {
+        ISuperRbac rbac = ISuperRbac(superRegistry.getAddress(keccak256("SUPER_RBAC_ID")));
+        if (!rbac.hasRole(keccak256("HOOKS_MANAGER_ROLE"), msg.sender)) revert NOT_AUTHORIZED();
+        _;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                 OWNER METHODS
+    //////////////////////////////////////////////////////////////*/
+    function registerHook(address hook_) external onlyHooksManager {
+        if (isHookRegistered[hook_]) revert HOOK_ALREADY_REGISTERED();
+        isHookRegistered[hook_] = true;
+        registeredHooks.push(hook_);
+    }
+
+    function unregisterHook(address hook_) external onlyHooksManager {
+        if (!isHookRegistered[hook_]) revert HOOK_NOT_REGISTERED();
+        isHookRegistered[hook_] = false;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                 VIEW METHODS
+    //////////////////////////////////////////////////////////////*/
+    function getRegisteredHooks() external view returns (address[] memory) {
+        return registeredHooks;
+    }
+}
+
