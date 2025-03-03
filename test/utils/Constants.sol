5cbf8c8790173356f398114537f3072c853ca50c
Tamara Ringas <tamriasdev@gmail.com>
2025-03-03 12:10:02 +0200

    feat: add gear and curve yield source keys

--- test/utils/Constants.sol
+++ test/utils/Constants.sol
@@ -87,18 +87,23 @@ abstract contract Constants {
     string public constant ERC7540_YIELD_SOURCE_ORACLE_KEY = "ERC7540YieldSourceOracle";
     string public constant FLUID_YIELD_SOURCE_ORACLE_KEY = "FluidYieldSourceOracle";
     string public constant PERIPHERY_REGISTRY_KEY = "PeripheryRegistry";
+
     // tokens
     string public constant DAI_KEY = "DAI";
     string public constant USDC_KEY = "USDC";
     string public constant WETH_KEY = "WETH";
     string public constant SUSDE_KEY = "SUSDe";
     string public constant USDCe_KEY = "USDCe";
+    string public constant GEAR_KEY = "GEAR";
+    string public constant CRV_USDC_KEY = "CRV-USDC";
 
     address public constant CHAIN_1_DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address public constant CHAIN_1_USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address public constant CHAIN_1_WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address public constant CHAIN_1_SUSDE = 0x9D39A5DE30e57443BfF2A8307A4256c8797A3497;
     address public constant CHAIN_1_USDE = 0x4c9EDD5852cd905f086C759E8383e09bff1E68B3;
+    address public constant CHAIN_1_GEAR = 0xBa3335588D9403515223F109EdC4eB7269a9Ab5D;
+    address public constant CHAIN_1_CRV_USDC = 0x8EF73f036fEEC873D0B2fd20892215Df5B8Bdd72;
 
     address public constant CHAIN_10_DAI = 0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1;
     address public constant CHAIN_10_USDC = 0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85;
@@ -122,6 +127,7 @@ abstract contract Constants {
     string public constant ALOE_USDC_VAULT_KEY = "AloeUSDC";
     string public constant FLUID_VAULT_KEY = "FluidVault";
     string public constant EULER_VAULT_KEY = "EulerVault";
+    string public constant CURVE_GEARBOX_VAULT_KEY = "CurveGearbox";
 
     string public constant MORPHO_VAULT_KEY = "MorphoVault";
     string public constant CENTRIFUGE_USDC_VAULT_KEY = "CentrifugeUSDC";
@@ -141,11 +147,17 @@ abstract contract Constants {
     address public constant CHAIN_1_YearnVault = 0x028eC7330ff87667b6dfb0D94b954c820195336c;
     address public constant CHAIN_1_GearboxVault = 0xda00000035fef4082F78dEF6A8903bee419FbF8E;
     address public constant CHAIN_1_PendleEthena = 0x3Ee118EFC826d30A29645eAf3b2EaaC9E8320185;
-
+    address public constant CHAIN_1_CurveGearbox = 0x8EF73f036fEEC873D0B2fd20892215Df5B8Bdd72
+    ;
     address public constant CHAIN_10_AloeUSDC = 0x462654Cc90C9124A406080EadaF0bA349eaA4AF9;
 
     address public constant CHAIN_8453_MorphoGauntletUSDCPrime = 0xeE8F4eC5672F09119b96Ab6fB59C27E1b7e44b61;
     address public constant CHAIN_8453_MorphoGauntletWETHCore = 0x6b13c060F13Af1fdB319F52315BbbF3fb1D88844;
+
+    // staking protocols
+    string public constant GEARBOX_STAKING_KEY = "GearboxStaking";
+
+    address public constant CHAIN_1_GearboxStaking = 	0x2fcbd02d5b1d52fc78d4c02890d7f4f47a459c33;
 
     // bridges
     string public constant DEBRIDGE_GATE_ADDRESS_KEY = "DeBridgeGateAddress";
