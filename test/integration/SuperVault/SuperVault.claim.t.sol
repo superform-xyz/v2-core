5b6a67d5bebcd986fc1300ee07376af934c3a63d
Tamara Ringas <tamriasdev@gmail.com>
2025-03-03 19:11:45 +0200

    feat: add internal helpers to get fulfill() and claim() working

--- test/integration/SuperVault/SuperVault.claim.t.sol
+++ test/integration/SuperVault/SuperVault.claim.t.sol
@@ -38,22 +38,27 @@ contract SuperVaultClaimTest is BaseSuperVaultTest {
     using Math for uint256;
 
     SuperVault gearSuperVault;
+    SuperVaultEscrow escrowGearSuperVault;
+    SuperVaultStrategy strategyGearSuperVault;
 
     // Yield sources
     IGearboxFarmingPool public curveGearboxFarmingPool;
+    IERC4626 public gearboxVault;
 
     uint256 public amount;
 
     uint256 public constant PRECISION = 1e18;
+    uint256 public constant GEAR_SUPERVAULT_ALLOCATION_RATE = 10_000; // 100%
 
     function setUp() public override {
         super.setUp();
 
-        console2.log("------setUp");
+        console2.log("------setUp Gearbox Claim Test");
 
         amount = 1000e6; // 1000 GEAR
 
         vm.selectFork(FORKS[ETH]);
+
         accInstances = randomAccountInstances[ETH];
         assertEq(accInstances.length, ACCOUNT_COUNT);
         peripheryRegistry = PeripheryRegistry(_getContract(ETH, PERIPHERY_REGISTRY_KEY));
@@ -72,28 +77,19 @@ contract SuperVaultClaimTest is BaseSuperVaultTest {
         vm.label(STRATEGIST, "STRATEGIST");
         vm.label(address(factory), "Factory");
 
-        // Set up super executor
-        // superExecutorOnEth = ISuperExecutor(_getContract(ETH, SUPER_EXECUTOR_KEY));
-
-        // Deploy factory
-        // factory = new SuperVaultFactory(_getContract(ETH, PERIPHERY_REGISTRY_KEY));
-
-        // Set up roles
-        // SV_MANAGER = _deployAccount(MANAGER_KEY, "SV_MANAGER");
-        // STRATEGIST = _deployAccount(STRATEGIST_KEY, "STRATEGIST");
-        // EMERGENCY_ADMIN = _deployAccount(EMERGENCY_ADMIN_KEY, "EMERGENCY_ADMIN");
-
         // Get USDC from fork
-        asset = IERC20Metadata(existingUnderlyingTokens[ETH][GEAR_KEY]);
+        asset = IERC20Metadata(existingUnderlyingTokens[ETH][USDC_KEY]);
         console2.log("asset: ", address(asset));
+
+        // Get real yield sources from fork
+        address gearboxVaultAddr = realVaultAddresses[ETH][ERC4626_VAULT_KEY][GEARBOX_VAULT_KEY][USDC_KEY];
+        console2.log("----gearboxVaultAddr: ", gearboxVaultAddr);
+        gearboxVault = IERC4626(gearboxVaultAddr);
+        vm.label(gearboxVaultAddr, "GearboxVault");
 
         address gearboxStakingAddr 
         = realVaultAddresses[ETH][GEARBOX_YIELD_SOURCE_ORACLE_KEY][GEARBOX_STAKING_KEY][GEAR_KEY];
         vm.label(gearboxStakingAddr, "GearboxStaking");
-
-        // Get real yield sources from fork
-        curveGearboxFarmingPool = IGearboxFarmingPool(gearboxStakingAddr);
-        vm.label(address(curveGearboxFarmingPool), "GearboxStaking"); 
 
         // Deploy vault trio with initial config
         ISuperVaultStrategy.GlobalConfig memory config = ISuperVaultStrategy.GlobalConfig({
@@ -104,33 +100,33 @@ contract SuperVaultClaimTest is BaseSuperVaultTest {
         });
         bytes32 hookRoot = _getMerkleRoot();
 
-        // vm.startPrank(SV_MANAGER);
-        // strategy.proposeOrExecuteHookRoot(hookRoot);
-        // vm.warp(block.timestamp + 7 days);
-        // strategy.proposeOrExecuteHookRoot(bytes32(0));
-        // vm.stopPrank();
-
+        address depositHookAddress = _getHookAddress(ETH, DEPOSIT_4626_VAULT_HOOK_KEY);
         address stakeHookAddress = _getHookAddress(ETH, GEARBOX_STAKE_HOOK_KEY);
         console2.log("stakeHookAddress: ", stakeHookAddress);
 
         address[] memory bootstrapHooks = new address[](1);
-        bootstrapHooks[0] = stakeHookAddress;
+        bootstrapHooks[0] = depositHookAddress;
+        //bootstrapHooks[1] = stakeHookAddress;
 
         bytes32[][] memory bootstrapHookProofs = new bytes32[][](1);
-        bootstrapHookProofs[0] = _getMerkleProof(stakeHookAddress);
+        bootstrapHookProofs[0] = _getMerkleProof(depositHookAddress);
+        //bootstrapHookProofs[1] = _getMerkleProof(stakeHookAddress);
 
         bytes[] memory bootstrapHooksData = new bytes[](1);
-        bootstrapHooksData[0] = _createGearboxStakeHookData(
-            bytes4(bytes(GEARBOX_YIELD_SOURCE_ORACLE_KEY)), 
-            address(curveGearboxFarmingPool),
+        bootstrapHooksData[0] = _createDeposit4626HookData(
+            bytes4(bytes(ERC4626_YIELD_SOURCE_ORACLE_KEY)), 
+            address(gearboxVault),
             BOOTSTRAP_AMOUNT, 
             false, 
             false
         );
-
-        // (bytes32 receivedRoot, , address[] memory hooksAddresses) = _generateMerkleTree();
-        // assertEq(receivedRoot, hookRoot);
-        // console2.log("hooksAddresses: ", hooksAddresses[0]);
+        // bootstrapHooksData[1] = _createGearboxStakeHookData(
+        //     bytes4(bytes(GEARBOX_YIELD_SOURCE_ORACLE_KEY)), 
+        //     address(curveGearboxFarmingPool),
+        //     BOOTSTRAP_AMOUNT, 
+        //     false, 
+        //     false
+        // );
 
         vm.startPrank(SV_MANAGER);
         deal(address(asset), SV_MANAGER, BOOTSTRAP_AMOUNT * 2);
@@ -147,11 +143,11 @@ contract SuperVaultClaimTest is BaseSuperVaultTest {
                 emergencyAdmin: EMERGENCY_ADMIN,
                 feeRecipient: TREASURY,
                 config: config,
-                finalMaxAllocationRate: MAX_ALLOCATION_RATE,
+                finalMaxAllocationRate: GEAR_SUPERVAULT_ALLOCATION_RATE,
                 bootstrapAmount: BOOTSTRAP_AMOUNT,
-                initYieldSource: address(curveGearboxFarmingPool),
+                initYieldSource: address(gearboxVault),
                 initHooksRoot: hookRoot,
-                initYieldSourceOracle: _getContract(ETH, GEARBOX_YIELD_SOURCE_ORACLE_KEY),
+                initYieldSourceOracle: _getContract(ETH, ERC4626_YIELD_SOURCE_ORACLE_KEY),
                 bootstrappingHooks: bootstrapHooks,
                 bootstrappingHookProofs: bootstrapHookProofs,
                 bootstrappingHookCalldata: bootstrapHooksData
@@ -163,25 +159,31 @@ contract SuperVaultClaimTest is BaseSuperVaultTest {
 
         // Cast addresses to contract types
         gearSuperVault = SuperVault(gearSuperVaultAddr);
-        escrow = SuperVaultEscrow(escrowAddr);
-        strategy = SuperVaultStrategy(strategyAddr);
+        escrowGearSuperVault = SuperVaultEscrow(escrowAddr);
+        strategyGearSuperVault = SuperVaultStrategy(strategyAddr);
 
         // Add a new yield source as manager
-        strategy.manageYieldSource(
-            address(curveGearboxFarmingPool),
-            _getContract(ETH, GEARBOX_YIELD_SOURCE_ORACLE_KEY),
-            0,
-            false // addYieldSource
-        );
+        // strategy.manageYieldSource(
+        //     address(gearboxVault),
+        //     _getContract(ETH, ERC4626_YIELD_SOURCE_ORACLE_KEY),
+        //     0,
+        //     false // addYieldSource
+        // );
+        // strategy.manageYieldSource(
+        //     address(curveGearboxFarmingPool),
+        //     _getContract(ETH, GEARBOX_YIELD_SOURCE_ORACLE_KEY),
+        //     0,
+        //     false // addYieldSource
+        // );
         vm.stopPrank();
 
         _setFeeConfig(100, TREASURY);
 
         // Set up hook root (same one as bootstrap, just to test)
         vm.startPrank(SV_MANAGER);
-        strategy.proposeOrExecuteHookRoot(hookRoot);
+        strategyGearSuperVault.proposeOrExecuteHookRoot(hookRoot);
         vm.warp(block.timestamp + 7 days);
-        strategy.proposeOrExecuteHookRoot(bytes32(0));
+        strategyGearSuperVault.proposeOrExecuteHookRoot(bytes32(0));
         vm.stopPrank();
     }
 
@@ -192,7 +194,7 @@ contract SuperVaultClaimTest is BaseSuperVaultTest {
         uint256 initialUserAssets = asset.balanceOf(accountEth);
 
         // Step 1: Request Deposit
-        _requestDeposit(amount);
+        __requestDeposit_Gearbox_SV(instanceOnEth, amount);
 
         // Verify assets transferred from user to vault
         assertEq(
@@ -200,39 +202,73 @@ contract SuperVaultClaimTest is BaseSuperVaultTest {
         );
 
         // Step 2: Fulfill Deposit
-        _fulfillDeposit_CRV_SV(amount);
+        _fulfillDeposit_Gearbox_SV(amount);
 
         // Step 3: Claim Deposit
-        _claimDeposit(amount);
+        __claimDeposit_Gearbox_SV(instanceOnEth, amount);
 
         // Get shares minted to user
         uint256 userShares = IERC20(vault.share()).balanceOf(accountEth);
     }
 
-    function _fulfillDeposit_CRV_SV(uint256 depositAmount) internal {
+    /*//////////////////////////////////////////////////////////////
+                        PRIVATE FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    function __requestDeposit_Gearbox_SV(AccountInstance memory accInst, uint256 depositAmount) private {
+        address[] memory hooksAddresses = new address[](2);
+        hooksAddresses[0] = _getHookAddress(ETH, APPROVE_ERC20_HOOK_KEY);
+        hooksAddresses[1] = _getHookAddress(ETH, REQUEST_DEPOSIT_7540_VAULT_HOOK_KEY);
+
+        bytes[] memory hooksData = new bytes[](2);
+        hooksData[0] = _createApproveHookData(address(asset), address(gearSuperVault), depositAmount, false);
+        hooksData[1] = _createRequestDeposit7540VaultHookData(
+            bytes4(bytes(ERC7540_YIELD_SOURCE_ORACLE_KEY)), address(gearSuperVault), accInst.account, depositAmount, false
+        );
+
+        ISuperExecutor.ExecutorEntry memory entry =
+            ISuperExecutor.ExecutorEntry({ hooksAddresses: hooksAddresses, hooksData: hooksData });
+        UserOpData memory userOpData = _getExecOps(accInst, superExecutorOnEth, abi.encode(entry));
+        executeOp(userOpData);
+    }
+
+    function _fulfillDeposit_Gearbox_SV(uint256 depositAmount) internal {
         address[] memory requestingUsers = new address[](1);
         requestingUsers[0] = accountEth;
-        address stakeHookAddress = _getHookAddress(ETH, GEARBOX_STAKE_HOOK_KEY);
-        console2.log("stakeHookAddress", stakeHookAddress);
+        address depositHookAddress = _getHookAddress(ETH, DEPOSIT_4626_VAULT_HOOK_KEY);
 
         address[] memory fulfillHooksAddresses = new address[](1);
-        fulfillHooksAddresses[0] = stakeHookAddress;
+        fulfillHooksAddresses[0] = depositHookAddress;
 
         bytes32[][] memory proofs = new bytes32[][](1);
-        proofs[0] = _getMerkleProof(stakeHookAddress);
+        proofs[0] = _getMerkleProof(depositHookAddress);
 
         bytes[] memory fulfillHooksData = new bytes[](1);
         // allocate up to the max allocation rate in the two Vaults
         fulfillHooksData[0] = _createGearboxStakeHookData(
-            bytes4(bytes(GEARBOX_YIELD_SOURCE_ORACLE_KEY)), address(curveGearboxFarmingPool), depositAmount, false, false
+            bytes4(bytes(ERC4626_YIELD_SOURCE_ORACLE_KEY)), address(gearboxVault), depositAmount, false, false
         );
 
         vm.startPrank(STRATEGIST);
-        strategy.fulfillRequests(requestingUsers, fulfillHooksAddresses, proofs, fulfillHooksData, true);
+        strategyGearSuperVault.fulfillRequests(requestingUsers, fulfillHooksAddresses, proofs, fulfillHooksData, true);
         vm.stopPrank();
 
         (uint256 pricePerShare) = _getSuperVaultPricePerShare();
         uint256 shares = depositAmount.mulDiv(PRECISION, pricePerShare);
         userSharePricePoints[accountEth].push(SharePricePoint({ shares: shares, pricePerShare: pricePerShare }));
+    }
+
+    function __claimDeposit_Gearbox_SV(AccountInstance memory accInst, uint256 depositAmount) private {
+        address[] memory claimHooksAddresses = new address[](1);
+        claimHooksAddresses[0] = _getHookAddress(ETH, DEPOSIT_7540_VAULT_HOOK_KEY);
+
+        bytes[] memory claimHooksData = new bytes[](1);
+        claimHooksData[0] = _createDeposit7540VaultHookData(
+            bytes4(bytes(ERC7540_YIELD_SOURCE_ORACLE_KEY)), address(gearSuperVault), accInst.account, depositAmount, false, false
+        );
+
+        ISuperExecutor.ExecutorEntry memory claimEntry =
+            ISuperExecutor.ExecutorEntry({ hooksAddresses: claimHooksAddresses, hooksData: claimHooksData });
+        UserOpData memory claimUserOpData = _getExecOps(accInst, superExecutorOnEth, abi.encode(claimEntry));
+        executeOp(claimUserOpData);
     }
 }
