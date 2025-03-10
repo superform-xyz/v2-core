e5fb176769fa678b2fc716bb65c23d2749f09581
Tamara Ringas <tamriasdev@gmail.com>
2025-03-10 16:20:10 +0200

    feat: add hook contract

---
+++ src/core/hooks/stake/gearbox/ApproveAndGearboxStakeHook.sol
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.28;
+
+// external
+import { BytesLib } from "../../../../vendor/BytesLib.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { Execution } from "modulekit/accounts/erc7579/lib/ExecutionLib.sol";
+
+// Superform
+import { BaseHook } from "../../BaseHook.sol";
+
+import { ISuperHook, ISuperHookResult, ISuperHookInflowOutflow } from "../../../interfaces/ISuperHook.sol";
+import { IGearboxFarmingPool } from "../../../../vendor/gearbox/IGearboxFarmingPool.sol";
+
+import { HookDataDecoder } from "../../../libraries/HookDataDecoder.sol";
+
+/// @title ApproveAndGearboxStakeHook
+/// @author Superform Labs
+/// @dev data has the following structure
+/// @notice         bytes4 yieldSourceOracleId = bytes4(BytesLib.slice(data, 0, 4), 0);
+/// @notice         address yieldSource = BytesLib.toAddress(BytesLib.slice(data, 4, 20), 0);
+/// @notice         address token = BytesLib.toAddress(BytesLib.slice(data, 24, 20), 0);
+/// @notice         uint256 amount = BytesLib.toUint256(BytesLib.slice(data, 44, 32), 0);
+/// @notice         bool usePrevHookAmount = _decodeBool(data, 76);
+/// @notice         bool lockForSP = _decodeBool(data, 77);
+contract ApproveAndGearboxStakeHook is BaseHook, ISuperHook, ISuperHookInflowOutflow {
+    using HookDataDecoder for bytes;
+
+    uint256 private constant AMOUNT_POSITION = 44;
+
+    constructor(address registry_, address author_) BaseHook(registry_, author_, HookType.INFLOW) { }
+
+    /*//////////////////////////////////////////////////////////////
+                                 VIEW METHODS
+    //////////////////////////////////////////////////////////////*/
+    /// @inheritdoc ISuperHook
+    function build(
+        address prevHook,
+        address,
+        bytes memory data
+    )
+        external
+        view
+        override
+        returns (Execution[] memory executions)
+    {
+        address yieldSource = data.extractYieldSource();
+        // yieldSource is spender for approval
+        address token = BytesLib.toAddress(BytesLib.slice(data, 24, 20), 0);
+        uint256 amount = _decodeAmount(data);
+        bool usePrevHookAmount = _decodeBool(data, 76);
+
+        if (yieldSource == address(0) || token == address(0)) revert ADDRESS_NOT_VALID();
+
+        if (usePrevHookAmount) {
+            amount = ISuperHookResult(prevHook).outAmount();
+        }
+
+        executions = new Execution[](4);
+        executions[0] =
+            Execution({ target: token, value: 0, callData: abi.encodeCall(IERC20.approve, (yieldSource, 0)) });
+        executions[1] =
+            Execution({ target: token, value: 0, callData: abi.encodeCall(IERC20.approve, (yieldSource, amount)) });
+        executions[2] = Execution({
+            target: yieldSource,
+            value: 0,
+            callData: abi.encodeCall(IGearboxFarmingPool.deposit, (amount))
+        });
+        executions[3] =
+            Execution({ target: token, value: 0, callData: abi.encodeCall(IERC20.approve, (yieldSource, 0)) });
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                 EXTERNAL METHODS
+    //////////////////////////////////////////////////////////////*/
+    /// @inheritdoc ISuperHook
+    function preExecute(address, address account, bytes memory data) external {
+        outAmount = _getBalance(account, data);
+        lockForSP = _decodeBool(data, 77);
+        spToken = data.extractYieldSource();
+    }
+
+    /// @inheritdoc ISuperHook
+    function postExecute(address, address account, bytes memory data) external {
+        outAmount = _getBalance(account, data) - outAmount;
+    }
+
+    /// @inheritdoc ISuperHookInflowOutflow
+    function decodeAmount(bytes memory data) external pure returns (uint256) {
+        return _decodeAmount(data);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                 PRIVATE METHODS
+    //////////////////////////////////////////////////////////////*/
+    function _decodeAmount(bytes memory data) private pure returns (uint256) {
+        return BytesLib.toUint256(BytesLib.slice(data, AMOUNT_POSITION, 32), 0);
+    }
+
+    function _getBalance(address account, bytes memory data) private view returns (uint256) {
+        return IGearboxFarmingPool(data.extractYieldSource()).balanceOf(account);
+    }
+}
