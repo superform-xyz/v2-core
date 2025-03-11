823b440309536814db883240f1251f2e3b383aef
Tamara Ringas <tamriasdev@gmail.com>
2025-03-11 14:20:11 +0200

    feat: add hook

---
+++ src/core/hooks/stake/fluid/ApproveAndFluidStakeHook.sol
@@ -0,0 +1,109 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.28;
+
+// external
+import { BytesLib } from "../../../../vendor/BytesLib.sol";
+import { Execution } from "modulekit/accounts/erc7579/lib/ExecutionLib.sol";
+import { IERC20 } from "openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+import { IFluidLendingStakingRewards } from "../../../../vendor/fluid/IFluidLendingStakingRewards.sol";
+
+// Superform
+import { BaseHook } from "../../BaseHook.sol";
+import { ISuperHook, ISuperHookResult, ISuperHookInflowOutflow } from "../../../interfaces/ISuperHook.sol";
+import { HookDataDecoder } from "../../../libraries/HookDataDecoder.sol";
+
+/// @title ApproveAndFluidStakeHook
+/// @author Superform Labs
+/// @dev data has the following structure
+/// @notice         bytes4 yieldSourceOracleId = bytes4(BytesLib.slice(data, 0, 4), 0);
+/// @notice         address yieldSource = BytesLib.toAddress(BytesLib.slice(data, 4, 20), 0);
+/// @notice         address token = BytesLib.toAddress(BytesLib.slice(data, 24, 20), 0);
+/// @notice         uint256 amount = BytesLib.toUint256(BytesLib.slice(data, 44, 32), 0);
+/// @notice         bool usePrevHookAmount = _decodeBool(data, 76);
+/// @notice         bool lockForSP = _decodeBool(data, 77);
+contract ApproveAndFluidStakeHook is BaseHook, ISuperHook, ISuperHookInflowOutflow {
+    using HookDataDecoder for bytes;
+
+    uint256 private constant AMOUNT_POSITION = 44;
+
+    constructor(address registry_, address author_) BaseHook(registry_, author_, HookType.INFLOW) { }
+
+    /*//////////////////////////////////////////////////////////////
+                                 VIEW METHODS
+    //////////////////////////////////////////////////////////////*/
+    /// @inheritdoc ISuperHook
+    function build(
+        address prevHook,
+        address,
+        bytes memory data
+    )
+        external
+        view
+        override
+        returns (Execution[] memory executions)
+    {
+        address yieldSource = data.extractYieldSource();
+        address token = BytesLib.toAddress(BytesLib.slice(data, 24, 20), 0);
+        uint256 amount = _decodeAmount(data);
+        bool usePrevHookAmount = _decodeBool(data, 76);
+
+        if (yieldSource == address(0) || token == address(0)) revert ADDRESS_NOT_VALID();
+
+        if (usePrevHookAmount) {
+            amount = ISuperHookResult(prevHook).outAmount();
+        }
+
+        executions = new Execution[](4);
+        executions[0] = Execution({
+          target: token,
+          value: 0,
+          callData: abi.encodeCall(IERC20.approve, (yieldSource, 0))
+        });
+        executions[1] = Execution({
+          target: token,
+          value: 0,
+          callData: abi.encodeCall(IERC20.approve, (yieldSource, amount))
+        });
+        executions[2] = Execution({
+            target: yieldSource,
+            value: 0,
+            callData: abi.encodeCall(IFluidLendingStakingRewards.stake, (amount))
+        });
+        executions[3] = Execution({
+          target: token,
+          value: 0,
+          callData: abi.encodeCall(IERC20.approve, (yieldSource, 0))
+        });
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                 EXTERNAL METHODS
+    //////////////////////////////////////////////////////////////*/
+    /// @inheritdoc ISuperHook
+    function preExecute(address, address account, bytes memory data) external {
+        outAmount = _getBalance(account, data);
+        lockForSP = _decodeBool(data, 57);
+        /// @dev in Fluid, the share token doesn't exist because no shares are minted so we don't assign a spToken
+    }
+
+    /// @inheritdoc ISuperHook
+    function postExecute(address, address account, bytes memory data) external {
+        outAmount = _getBalance(account, data) - outAmount;
+    }
+
+    /// @inheritdoc ISuperHookInflowOutflow
+    function decodeAmount(bytes memory data) external pure returns (uint256) {
+        return _decodeAmount(data);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                 PRIVATE METHODS
+    //////////////////////////////////////////////////////////////*/
+    function _decodeAmount(bytes memory data) private pure returns (uint256) {
+        return BytesLib.toUint256(BytesLib.slice(data, AMOUNT_POSITION, 32), 0);
+    }
+
+    function _getBalance(address account, bytes memory data) private view returns (uint256) {
+        return IFluidLendingStakingRewards(data.extractYieldSource()).balanceOf(account);
+    }
+}
