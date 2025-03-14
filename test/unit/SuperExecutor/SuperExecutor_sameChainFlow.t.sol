669b223f3120825276f426de0118e8b5ea0d050f
Cosmin <cosmin@superform.xyz>
2025-03-14 15:28:42 +0200

    feat: test 7702 with SuperExecutor (#210)

--- test/unit/SuperExecutor/SuperExecutor_sameChainFlow.t.sol
+++ test/unit/SuperExecutor/SuperExecutor_sameChainFlow.t.sol
@@ -2,7 +2,8 @@
 pragma solidity >=0.8.28;
 
 // external
-import { AccountInstance, UserOpData } from "modulekit/ModuleKit.sol";
+import { RhinestoneModuleKit, ModuleKitHelpers, AccountInstance, UserOpData } from "modulekit/ModuleKit.sol";
+import { ExecutionLib } from "modulekit/accounts/erc7579/lib/ExecutionLib.sol";
 import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 
 // Superform
@@ -21,11 +22,25 @@ import { SuperRegistry } from "../../../src/core/settings/SuperRegistry.sol";
 import { BaseTest } from "../../BaseTest.t.sol";
 import { ExecutionReturnData } from "modulekit/test/RhinestoneModuleKit.sol";
 import { BytesLib } from "../../../src/vendor/BytesLib.sol";
-
 import "forge-std/console.sol";
+
+import { IERC7579Account } from "modulekit/accounts/common/interfaces/IERC7579Account.sol";
+import { ExecLib } from "modulekit/accounts/kernel/lib/ExecLib.sol";
+import { ModeLib, ModeCode } from "modulekit/accounts/common/lib/ModeLib.sol";
+import { CallType, ExecType, ExecMode, ExecLib } from "modulekit/accounts/kernel/lib/ExecLib.sol";
+import { Execution } from "modulekit/accounts/erc7579/lib/ExecutionLib.sol";
+
+import "modulekit/test/RhinestoneModuleKit.sol";
+import { ERC7579Precompiles } from "modulekit/deployment/precompiles/ERC7579Precompiles.sol";
+import "modulekit/accounts/erc7579/ERC7579Factory.sol";
+import { MODULE_TYPE_EXECUTOR } from "modulekit/accounts/kernel/types/Constants.sol";
+
+import { ECDSA } from "openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
+
+
 import { Vm } from "forge-std/Test.sol";
 
-contract SuperExecutor_sameChainFlow is BaseTest {
+contract SuperExecutor_sameChainFlow is BaseTest, ERC7579Precompiles {
     using BytesLib for bytes;
     using AddressLib for Address;
 
@@ -38,6 +53,12 @@ contract SuperExecutor_sameChainFlow is BaseTest {
     ISuperExecutor public superExecutor;
     SuperRegistry public superRegistry;
     MockSuperPositionFactory public mockSuperPositionFactory;
+
+    uint256 eoaKey;
+    address account7702;    
+    ERC7579Factory erc7579factory;
+    IERC7579Account erc7579account;
+    IERC7579Bootstrap bootstrapDefault;
 
     function setUp() public override {
         super.setUp();
@@ -53,6 +74,19 @@ contract SuperExecutor_sameChainFlow is BaseTest {
         superRegistry = SuperRegistry(_getContract(ETH, SUPER_REGISTRY_KEY));
         mockSuperPositionFactory = new MockSuperPositionFactory(address(this));
         vm.label(address(mockSuperPositionFactory), "MockSuperPositionFactory");
+
+        eoaKey = uint256(8);
+        account7702 = vm.addr(eoaKey);
+        vm.label(account7702, "7702CompliantAccount");
+        vm.deal(account7702, LARGE);
+
+        erc7579factory = new ERC7579Factory();
+        erc7579account = deployERC7579Account();
+        assertGt(address(erc7579account).code.length, 0);
+        vm.label(address(erc7579account), "ERC7579Account");
+
+        bootstrapDefault = deployERC7579Bootstrap();
+        vm.label(address(bootstrapDefault), "ERC7579Bootstrap");
     }
 
     function test_ShouldExecuteAll(uint256 amount) external {
@@ -320,5 +354,133 @@ contract SuperExecutor_sameChainFlow is BaseTest {
                 }
             }
         }
+    }
+
+    struct Test7579MethodsVars {
+        uint256 amount;
+        AccountInstance instance;
+        bytes setValueCalldata;
+        bytes userOpCalldata;
+        uint192 key;
+        uint256 nonce;
+        bytes signature;
+        PackedUserOperation[] userOps;
+        bool success;
+        bytes result;
+        bool opsSuccess;
+        bytes opsResult;
+    }
+    function test_7702_SuperExecutor(uint256 amount)
+        external 
+        add7702Precompile(account7702, address(erc7579account).code)
+    {
+        Test7579MethodsVars memory vars;
+        vars.instance = instance;
+        amount = _bound(amount);
+
+        // prepare useOp
+        address[] memory hooksAddresses = new address[](2);
+        hooksAddresses[0] = _getHookAddress(ETH, APPROVE_ERC20_HOOK_KEY);
+        hooksAddresses[1] = _getHookAddress(ETH, DEPOSIT_4626_VAULT_HOOK_KEY);
+
+        bytes[] memory hooksData = new bytes[](2);
+        hooksData[0] = _createApproveHookData(underlying, yieldSourceAddress, amount, false);
+        hooksData[1] = _createDeposit4626HookData(
+            bytes4(bytes(ERC4626_YIELD_SOURCE_ORACLE_KEY)), yieldSourceAddress, amount, false, false
+        );
+
+        // assure account has tokens
+        _getTokens(underlying, account7702, amount);
+
+        ISuperExecutor.ExecutorEntry memory entry =
+            ISuperExecutor.ExecutorEntry({ hooksAddresses: hooksAddresses, hooksData: hooksData });
+        UserOpData memory userOpData = _getExecOps(instance, superExecutor, abi.encode(entry));
+
+        //bytes memory initData = _get7702InitDataWithExecutor(address(_defaultValidator), ""); 
+        bytes memory initData = _get7702InitData(); 
+        Execution[] memory executions = new Execution[](3);
+        executions[0] = Execution({
+            target: account7702,
+            value: 0,
+            callData: abi.encodeCall(IMSA.initializeAccount, initData)
+        });
+        executions[1] = Execution({ target: account7702, value: 0, callData: abi.encodeCall(IERC7579Account.installModule, (MODULE_TYPE_EXECUTOR, address(superExecutor), "")) });
+        executions[2] = Execution({ target: address(superExecutor), value: 0, callData: abi.encodeCall(ISuperExecutor.execute, (abi.encode(entry))) });
+
+
+        vars.userOpCalldata = abi.encodeCall(
+            IERC7579Account.execute,
+            (ModeLib.encodeSimpleBatch(), ExecutionLib.encodeBatch(executions))
+        );
+
+        vars.key = uint192(bytes24(bytes20(address(_defaultValidator))));
+        vars.nonce = vars.instance.aux.entrypoint.getNonce(address(account7702), vars.key);
+
+        // prepare PackedUserOperation
+        vars.userOps = new PackedUserOperation[](1);
+        vars.userOps[0] = _getDefaultUserOp();
+        vars.userOps[0].sender = account7702;
+        vars.userOps[0].nonce = vars.nonce;
+        vars.userOps[0].callData = vars.userOpCalldata;
+        vars.userOps[0].signature = _getSignature(vars.userOps[0], vars.instance.aux.entrypoint);
+
+        assertGt(account7702.code.length, 0);
+        
+        vars.instance.aux.entrypoint.handleOps(vars.userOps, payable(address(0x69)));
+     
+        uint256 accSharesAfter = vaultInstance.balanceOf(account7702);
+        assertGt(accSharesAfter, 0);
+    }
+    function _get7702InitData() internal view returns (bytes memory) {
+        bytes memory initData = erc7579factory.getInitData(address(_defaultValidator), "");
+        return initData;
+    }
+
+    function _get7702InitDataWithExecutor(
+        address validator,
+        bytes memory initData
+    )
+        public
+        view
+        returns (bytes memory _init)
+    {
+        ERC7579BootstrapConfig[] memory _validators = new ERC7579BootstrapConfig[](1);
+        _validators[0].module = validator;
+        _validators[0].data = initData;
+        ERC7579BootstrapConfig[] memory _executors = new ERC7579BootstrapConfig[](1);
+        _executors[0].module = address(superExecutor);
+
+        ERC7579BootstrapConfig memory _hook;
+
+        ERC7579BootstrapConfig[] memory _fallBacks = new ERC7579BootstrapConfig[](0);
+        _init = abi.encode(
+            address(bootstrapDefault),
+            abi.encodeCall(IERC7579Bootstrap.initMSA, (_validators, _executors, _hook, _fallBacks))
+        );
+    }
+    function _getDefaultUserOp() internal pure returns (PackedUserOperation memory userOp) {
+        userOp = PackedUserOperation({
+            sender: address(0),
+            nonce: 0,
+            initCode: "",
+            callData: "",
+            accountGasLimits: bytes32(abi.encodePacked(uint128(2e6), uint128(2e6))),
+            preVerificationGas: 2e6,
+            gasFees: bytes32(abi.encodePacked(uint128(2e6), uint128(2e6))),
+            paymasterAndData: bytes(""),
+            signature: abi.encodePacked(hex"41414141")
+        });
+    }
+    function _getSignature(
+        PackedUserOperation memory userOp,
+        IEntryPoint entrypoint
+    )
+        internal
+        view
+        returns (bytes memory)
+    {
+        bytes32 hash = entrypoint.getUserOpHash(userOp);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(eoaKey, _toEthSignedMessageHash(hash));
+        return abi.encodePacked(r, s, v);
     }
 }
