0db73d4921909345851c50eb15db7c300a696d87
Nicola Bernini <nicola.bernini@gmail.com>
2025-03-17 22:18:11 +0100

    Removing derived implementation since the AVG CostBasis Logic is now inside BaseLedger

--- src/core/accounting/SuperLedgerOptimizedAVG.sol
+++ src/core/accounting/SuperLedgerOptimizedAVG.sol
@@ -32,76 +32,76 @@ contract SuperLedgerOptimizedAVG is BaseLedger {
 
 //    mapping(address user => mapping(address yieldSource => LedgerAlternative ledger)) internal userLedgerAlternative;
 
-    mapping(address user => uint256) public usersAccumulatorShares;
-    mapping(address user => uint256) public usersAccumulatorCostBasis;
+//    mapping(address user => uint256) public usersAccumulatorShares;
+//    mapping(address user => uint256) public usersAccumulatorCostBasis;
 
 
     /*//////////////////////////////////////////////////////////////
                         PRIVATE FUNCTIONS
 //////////////////////////////////////////////////////////////*/
 
-    function _takeSnapshot(
-        address user,
-        address yieldSource,
-        uint256 amountShares,
-        uint256 pps,
-        uint256 decimals
-    ) override internal {
-        usersAccumulatorShares[user] += amountShares;
-        usersAccumulatorCostBasis[user] += amountShares * pps / (10 ** decimals);
-    }
-
-    function _calculateAvgCostBasisView(
-        address user,
-        address yieldSource,
-        uint256 amountAssets,
-        uint256 usedShares,
-        ISuperLedgerConfiguration.YieldSourceOracleConfig memory config
-    ) internal view returns (uint256 costBasis) {
-        uint256 accumulatorShares = usersAccumulatorShares[user];
-        uint256 accumulatorCostBasis = usersAccumulatorCostBasis[user];
-
-        // TODO: Check if this is a good error message
-        if(usedShares > accumulatorShares) revert("INSUFFICIENT_SHARES");
-
-        // avgEntryPrice = accumulatorCostBasis / accumulatorShares
-        // TODO: Adjust precision?
-        costBasis = accumulatorCostBasis * usedShares / accumulatorShares;
-    }
-
-    function _calculateAvgCostBasis(
-        address user,
-        address yieldSource,
-        uint256 amountAssets,
-        uint256 usedShares,
-        ISuperLedgerConfiguration.YieldSourceOracleConfig memory config
-    ) internal returns (uint256 costBasis) {
-        costBasis = _calculateAvgCostBasisView(user, yieldSource, amountAssets, usedShares, config);
-
-        usersAccumulatorShares[user] -= usedShares;
-        usersAccumulatorCostBasis[user] -= costBasis;
-    }
-
-    function calculateCostBasisView(
-        address user,
-        address yieldSource,
-        uint256 amountAssets,
-        uint256 usedShares,
-        ISuperLedgerConfiguration.YieldSourceOracleConfig memory config
-    ) override public view
-    returns (uint256 costBasis) {
-        costBasis = _calculateAvgCostBasisView(user, yieldSource, amountAssets, usedShares, config);
-    }
-
-    function _calculateCostBasis(
-        address user,
-        address yieldSource,
-        uint256 amountAssets,
-        uint256 usedShares,
-        ISuperLedgerConfiguration.YieldSourceOracleConfig memory config
-    ) override internal
-    returns (uint256 costBasis) {
-        costBasis = _calculateAvgCostBasis(user, yieldSource, amountAssets, usedShares, config);
-    }
+//    function _takeSnapshot(
+//        address user,
+//        address yieldSource,
+//        uint256 amountShares,
+//        uint256 pps,
+//        uint256 decimals
+//    ) override internal {
+//        usersAccumulatorShares[user] += amountShares;
+//        usersAccumulatorCostBasis[user] += amountShares * pps / (10 ** decimals);
+//    }
+//
+//    function _calculateAvgCostBasisView(
+//        address user,
+//        address yieldSource,
+//        uint256 amountAssets,
+//        uint256 usedShares,
+//        ISuperLedgerConfiguration.YieldSourceOracleConfig memory config
+//    ) internal view returns (uint256 costBasis) {
+//        uint256 accumulatorShares = usersAccumulatorShares[user];
+//        uint256 accumulatorCostBasis = usersAccumulatorCostBasis[user];
+//
+//        // TODO: Check if this is a good error message
+//        if(usedShares > accumulatorShares) revert("INSUFFICIENT_SHARES");
+//
+//        // avgEntryPrice = accumulatorCostBasis / accumulatorShares
+//        // TODO: Adjust precision?
+//        costBasis = accumulatorCostBasis * usedShares / accumulatorShares;
+//    }
+//
+//    function _calculateAvgCostBasis(
+//        address user,
+//        address yieldSource,
+//        uint256 amountAssets,
+//        uint256 usedShares,
+//        ISuperLedgerConfiguration.YieldSourceOracleConfig memory config
+//    ) internal returns (uint256 costBasis) {
+//        costBasis = _calculateAvgCostBasisView(user, yieldSource, amountAssets, usedShares, config);
+//
+//        usersAccumulatorShares[user] -= usedShares;
+//        usersAccumulatorCostBasis[user] -= costBasis;
+//    }
+//
+//    function calculateCostBasisView(
+//        address user,
+//        address yieldSource,
+//        uint256 amountAssets,
+//        uint256 usedShares,
+//        ISuperLedgerConfiguration.YieldSourceOracleConfig memory config
+//    ) override public view
+//    returns (uint256 costBasis) {
+//        costBasis = _calculateAvgCostBasisView(user, yieldSource, amountAssets, usedShares, config);
+//    }
+//
+//    function _calculateCostBasis(
+//        address user,
+//        address yieldSource,
+//        uint256 amountAssets,
+//        uint256 usedShares,
+//        ISuperLedgerConfiguration.YieldSourceOracleConfig memory config
+//    ) override internal
+//    returns (uint256 costBasis) {
+//        costBasis = _calculateAvgCostBasis(user, yieldSource, amountAssets, usedShares, config);
+//    }
 
 }
