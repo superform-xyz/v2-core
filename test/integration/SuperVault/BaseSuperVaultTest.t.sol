dc4e0143bc0fbf9d9e19df87bcde48a0fe834baf
Tamara Ringas <tamriasdev@gmail.com>
2025-03-26 18:38:41 +0200

    fix: typo

--- test/integration/SuperVault/BaseSuperVaultTest.t.sol
+++ test/integration/SuperVault/BaseSuperVaultTest.t.sol
@@ -201,7 +201,7 @@ contract BaseSuperVaultTest is BaseTest, MerkleReader {
         bytes[] memory hooksData = new bytes[](2);
         hooksData[0] = _createApproveHookData(address(asset), address(vault), depositAmount, false);
         hooksData[1] = _createApproveAndRequestDeposit7540HookData(
-            bytes4(bytes(ERC7540_YIELD_SOURCE_ORACLE_KEY)), address(vault), depositAmount, false, false
+            bytes4(bytes(ERC7540_YIELD_SOURCE_ORACLE_KEY)), address(vault), address(asset), depositAmount, false
         );
 
         ISuperExecutor.ExecutorEntry memory entry =
