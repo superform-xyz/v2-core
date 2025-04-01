b1ec72c68cba1ea270c8d422509b86363722904c
Tamara Ringas <tamriasdev@gmail.com>
2025-04-01 17:10:23 +0200

    feat: add natspec and data outline

--- src/core/hooks/borrow/MorphoBorrowHook.sol
+++ src/core/hooks/borrow/MorphoBorrowHook.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.28;
 
 // external
 import { BytesLib } from "../../../../vendor/BytesLib.sol";
+import { IMorpho } from "../../../../vendor/morpho/IMorpho.sol";~~~~~~~~~~~~~~~
 import { Execution } from "modulekit/accounts/erc7579/lib/ExecutionLib.sol";
 
 // Superform
@@ -12,8 +13,15 @@ import { ISuperHook } from "../../../interfaces/ISuperHook.sol";
 /// @title MorphoBorrowHook
 /// @author Superform Labs
 /// @dev data has the following structure
-
-
+/// @notice         bytes4 yieldSourceOracleId = bytes4(BytesLib.slice(data, 0, 4), 0);
+/// @notice         address yieldSource = BytesLib.toAddress(BytesLib.slice(data, 4, 20), 0);
+/// @notice         address loanToken = BytesLib.toAddress(BytesLib.slice(data, 24, 20), 0);
+/// @notice         address collateralToken = BytesLib.toAddress(BytesLib.slice(data, 44, 20), 0);
+/// TODO: Does user specify oracle or we always use the same one?
+/// @notice         address oracle = BytesLib.toAddress(BytesLib.slice(data, 64, 20), 0);
+/// @notice         address irm = BytesLib.toAddress(BytesLib.slice(data, 84, 20), 0);
+/// @notice         uint256 assets = BytesLib.toUint256(BytesLib.slice(data, 104, 32), 0);
+/// @notice         uint256 lltv = BytesLib.toUint256(BytesLib.slice(data, 136, 32), 0);
 contract MorphoBorrowHook is BaseHook, ISuperHook {
     constructor(address registry_) BaseHook(registry_, HookType.NONACCOUNTING) { }
 
