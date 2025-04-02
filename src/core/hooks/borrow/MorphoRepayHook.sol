15032037cbbd470626dc8b11683164062aed576a
Tamara Ringas <tamriasdev@gmail.com>
2025-04-02 12:29:55 +0200

    feat: implement RepayHook

--- src/core/hooks/borrow/MorphoRepayHook.sol
+++ src/core/hooks/borrow/MorphoRepayHook.sol
@@ -1,56 +1,134 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity 0.8.28;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
 
-// // external
-// import { BytesLib } from "../../../vendor/BytesLib.sol";
-// import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-// import { IMorpho, MarketParams } from "../../../vendor/morpho/IMorpho.sol";
-// import { Execution } from "modulekit/accounts/erc7579/lib/ExecutionLib.sol";
+// external
+import { BytesLib } from "../../../vendor/BytesLib.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { IMorphoBase, MarketParams } from "../../../vendor/morpho/IMorpho.sol";
+import { Execution } from "modulekit/accounts/erc7579/lib/ExecutionLib.sol";
 
-// // Superform
-// import { BaseHook } from "../BaseHook.sol";
-// import { ISuperHook } from "../../interfaces/ISuperHook.sol";
-// import { HookDataDecoder } from "../../libraries/HookDataDecoder.sol";
+// Superform
+import { BaseHook } from "../BaseHook.sol";
+import { ISuperHook } from "../../interfaces/ISuperHook.sol";
+import { HookDataDecoder } from "../../libraries/HookDataDecoder.sol";
 
-// /// @title MorphoRepayHook
-// /// @author Superform Labs
-// /// @dev data has the following structure
-// /// @notice         address loanToken = BytesLib.toAddress(BytesLib.slice(data, 0, 20), 0);
-// /// @notice         address collateralToken = BytesLib.toAddress(BytesLib.slice(data, 20, 20), 0);
-// /// @notice         address oracle = BytesLib.toAddress(BytesLib.slice(data, 40, 20), 0);
-// /// @notice         address irm = BytesLib.toAddress(BytesLib.slice(data, 60, 20), 0);
-// /// @notice         uint256 lltv = BytesLib.toUint256(BytesLib.slice(data, 80, 32), 0);
-// /// @notice         bool usePrevHookAmount = _decodeBool(data, 112);
-// contract MorphoRepayHook is BaseHook, ISuperHook {
-//     using HookDataDecoder for bytes;
+/// @title MorphoRepayHook
+/// @author Superform Labs
+/// @dev data has the following structure
+/// @dev perfroms a full repayment of the loan token
+/// @notice         address loanToken = BytesLib.toAddress(BytesLib.slice(data, 0, 20), 0);
+/// @notice         address collateralToken = BytesLib.toAddress(BytesLib.slice(data, 20, 20), 0);
+/// @notice         address oracle = BytesLib.toAddress(BytesLib.slice(data, 40, 20), 0);
+/// @notice         address irm = BytesLib.toAddress(BytesLib.slice(data, 60, 20), 0);
+/// @notice         uint256 lltv = BytesLib.toUint256(BytesLib.slice(data, 80, 32), 0);
+contract MorphoRepayHook is BaseHook, ISuperHook {
+    using HookDataDecoder for bytes;
 
-//     /*//////////////////////////////////////////////////////////////
-//                                STORAGE
-//     //////////////////////////////////////////////////////////////*/
-//     address public morpho;
-//     IMorpho public morphoInterface;
-//     MarketParams public marketParams;
+    /*//////////////////////////////////////////////////////////////
+                               STORAGE
+    //////////////////////////////////////////////////////////////*/
+    address public morpho;
+    IMorphoBase public morphoInterface;
 
-//     uint256 private constant ASSETS_POSITION = 80;
+    struct BuildHookLocalVars {
+        address loanToken;
+        address collateralToken;
+        address oracle;
+        address irm;
+        uint256 lltv;
+    }
 
-//     /*//////////////////////////////////////////////////////////////
-//                               CONSTRUCTOR
-//     //////////////////////////////////////////////////////////////*/
-//     constructor(address registry_, address morpho_) BaseHook(registry_, HookType.NONACCOUNTING) {
-//         if (morpho_ == address(0)) revert ZERO_ADDRESS();
-//         morpho = morpho_;
-//         morphoInterface = IMorpho(morpho_);
-//     }
+    /*//////////////////////////////////////////////////////////////
+                              CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+    constructor(address registry_, address morpho_) BaseHook(registry_, HookType.NONACCOUNTING) {
+        if (morpho_ == address(0)) revert ZERO_ADDRESS();
+        morpho = morpho_;
+        morphoInterface = IMorphoBase(morpho_);
+    }
 
-//     /*//////////////////////////////////////////////////////////////
-//                               VIEW METHODS
-//     //////////////////////////////////////////////////////////////*/
+    /*//////////////////////////////////////////////////////////////
+                              VIEW METHODS
+    //////////////////////////////////////////////////////////////*/
+    /// @inheritdoc ISuperHook
+    function build(
+        address,
+        address account,
+        bytes memory data
+    )
+        external
+        view
+        override
+        returns (Execution[] memory executions)
+    {
+        BuildHookLocalVars memory vars = _decodeHookData(data);
 
-//     /*//////////////////////////////////////////////////////////////
-//                             EXTERNAL METHODS
-//     //////////////////////////////////////////////////////////////*/
+        if (vars.loanToken == address(0) || vars.collateralToken == address(0)) revert ADDRESS_NOT_VALID();
 
-//     /*//////////////////////////////////////////////////////////////
-//                             INTERNAL METHODS
-//     //////////////////////////////////////////////////////////////*/
-// }
+        MarketParams memory marketParams = _generateMarketParams(vars.loanToken, vars.collateralToken, vars.oracle, vars.irm, vars.lltv);
+
+        uint256 tokenBalance = _getBalance(account, data);
+        uint256 shareBalance = _deriveShareBalance(tokenBalance);
+
+        executions = new Execution[](3);
+        executions[0] = Execution({ target: vars.loanToken, value: 0, callData: abi.encodeCall(IERC20.approve, (morpho, 0)) });
+        executions[1] = Execution({ target: vars.loanToken, value: 0, callData: abi.encodeCall(IERC20.approve, (morpho, tokenBalance)) });
+        executions[2] = Execution({ target: morpho, value: 0, callData: abi.encodeCall(IMorphoBase.repay, (marketParams, 0, shareBalance, account, "")) });
+
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            EXTERNAL METHODS
+    //////////////////////////////////////////////////////////////*/
+    /// @inheritdoc ISuperHook
+    function preExecute(address, address account, bytes memory data) external {
+        // store current balance
+        outAmount = _getBalance(account, data);
+    }
+
+    // TODO: check if this is correct
+    /// @inheritdoc ISuperHook
+    function postExecute(address, address account, bytes memory data) external {
+        outAmount = _getBalance(account, data) - outAmount;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            INTERNAL METHODS
+    //////////////////////////////////////////////////////////////*/
+    function _decodeHookData(bytes memory data) internal pure returns (BuildHookLocalVars memory vars) {
+        address loanToken = BytesLib.toAddress(BytesLib.slice(data, 0, 20), 0);
+        address collateralToken = BytesLib.toAddress(BytesLib.slice(data, 20, 20), 0);
+        address oracle = BytesLib.toAddress(BytesLib.slice(data, 40, 20), 0);
+        address irm = BytesLib.toAddress(BytesLib.slice(data, 60, 20), 0);
+        uint256 lltv = BytesLib.toUint256(BytesLib.slice(data, 112, 32), 0);
+        vars = BuildHookLocalVars({ 
+            loanToken: loanToken, 
+            collateralToken: collateralToken, 
+            oracle: oracle, 
+            irm: irm, 
+            lltv: lltv 
+        });
+    }
+
+    function _generateMarketParams(address loanToken, address collateralToken, address oracle, address irm, uint256 lltv) internal pure returns (MarketParams memory) {
+        return MarketParams({
+            loanToken: loanToken,
+            collateralToken: collateralToken,
+            oracle: oracle,
+            irm: irm,
+            lltv: lltv
+        });
+    }
+
+    function _deriveShareBalance(uint256 tokenBalance) internal view returns (uint256) {
+        // return morphoInterface.getShares(marketParams, tokenBalance);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                 PRIVATE METHODS
+    //////////////////////////////////////////////////////////////*/
+    function _getBalance(address account, bytes memory data) private view returns (uint256) {
+        address loanToken = BytesLib.toAddress(BytesLib.slice(data, 0, 20), 0);
+        return IERC20(loanToken).balanceOf(account);
+    }
+}
