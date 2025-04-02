916867a85bc59f3599d73778cd1c8ff5ef1da55b
Tamara Ringas <tamriasdev@gmail.com>
2025-04-02 12:33:00 +0200

    fix: rm unused interface

--- src/core/hooks/borrow/MorphoRepayHook.sol
+++ src/core/hooks/borrow/MorphoRepayHook.sol
@@ -95,7 +95,7 @@ contract MorphoRepayHook is BaseHook, ISuperHook {
         outAmount = _getBalance(account, data);
     }
 
-    // TODO: check if this is correct
+    // TODO: check if this is correct, should the balances be swapped?
     /// @inheritdoc ISuperHook
     function postExecute(address, address account, bytes memory data) external {
         outAmount = _getBalance(account, data) - outAmount;
