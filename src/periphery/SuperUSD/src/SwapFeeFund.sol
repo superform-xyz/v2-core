b7fe163e0cde3e3c5adb7acbf27ba2f8314498d9
Nicola Bernini <nicola.bernini@gmail.com>
2025-04-17 16:26:46 +0200

    Add SwapFeeFund.sol

---
+++ src/periphery/SuperUSD/src/SwapFeeFund.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+
+/**
+ * @title Swap Fee Fund
+ * @notice Manages swap fee tokens.
+ */
+contract SwapFeeFund is AccessControl{
+
+    // --- Events ---
+    event RebalanceWithdrawal(address receiver, address tokenOut, uint256 amount);
+
+    // --- Constructor ---
+    constructor() {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(INCENTIVE_FUND_MANAGER, msg.sender);
+    }
+
+    // --- State Changing Functions ---
+
+    /**
+     * @notice Withdraws tokens from the fund.
+     * @param receiver The address to receive the tokens.
+     * @param tokenOut The token to withdraw.
+     * @param amount The amount to withdraw.
+     */
+    function withdraw(address receiver, address tokenOut, uint256 amount)
+    external
+    onlyRole(INCENTIVE_FUND_MANAGER)
+    {
+        require(receiver != address(0), "SwapFeeFund: Receiver cannot be zero address");
+        require(tokenOut != address(0), "SwapFeeFund: TokenOut cannot be zero address");
+
+        IERC20(tokenOut).transferFrom(address(this), receiver, amount);
+        emit RebalanceWithdrawal(receiver, tokenOut, amount);
+    }
+}
+
+
+
+
