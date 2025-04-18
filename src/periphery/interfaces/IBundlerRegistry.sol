95ed196bde98909e433bf73a7ab586647b1ec783
Cosmin <cosmin@superform.xyz>
2025-04-18 15:11:06 +0300

    feat: added bundler registry (#370)

---
+++ src/periphery/interfaces/IBundlerRegistry.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+interface IBundlerRegistry {
+    /*//////////////////////////////////////////////////////////////
+                                STRUCTS
+    //////////////////////////////////////////////////////////////*/
+    struct Bundler {
+        uint256 id; //unique identifier for the bundler
+        address bundlerAddress; //address of the bundler
+        bool isActive; //whether the bundler is active
+        bytes extraData; //extra data for off-chain use
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                EVENTS
+    //////////////////////////////////////////////////////////////*/
+    event BundlerRegistered(uint256 indexed id, address indexed bundler);
+    event BundlerAddressUpdated(uint256 indexed id, address indexed oldBundler, address indexed newBundler);
+    event BundlerExtraDataUpdated(uint256 indexed id, address indexed bundler, bytes extraData);
+    event BundlerStatusChanged(uint256 indexed id, address indexed bundler, bool isActive);
+
+    /*//////////////////////////////////////////////////////////////
+                                VIEW FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    /// @notice Check if a bundler is registered
+    /// @param bundler The address of the bundler
+    /// @return True if the bundler is registered, false otherwise
+    function isBundlerRegistered(address bundler) external view returns (bool);
+
+    /// @notice Check if a bundler is active
+    /// @param bundler The address of the bundler
+    /// @return True if the bundler is active, false otherwise
+    function isBundlerActive(address bundler) external view returns (bool);
+
+    /// @notice Get a bundler by its id
+    /// @param _bundlerId The id of the bundler
+    /// @return The bundler
+    function getBundler(uint256 _bundlerId) external view returns (Bundler memory);
+
+    /// @notice Get a bundler by its address
+    /// @param _addr The address of the bundler
+    /// @return The bundler
+    function getBundlerByAddress(address _addr) external view returns (Bundler memory);
+}
