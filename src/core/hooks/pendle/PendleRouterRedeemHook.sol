42236e6fd184be1871b05208a1e73ce078e35eb6
Tamara Ringas <tamriasdev@gmail.com>
2025-04-18 17:43:29 +0200

    feat: create TokenOutput using helper

--- src/core/hooks/pendle/PendleRouterRedeemHook.sol
+++ src/core/hooks/pendle/PendleRouterRedeemHook.sol
@@ -4,52 +4,28 @@ pragma solidity 0.8.28;
 // external
 import { Execution } from "modulekit/accounts/erc7579/lib/ExecutionLib.sol";
 import { IERC20 } from "openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+import { IPendleRouterV4, TokenOutput } from "../../../vendor/pendle/IPendleRouterV4.sol";
 
 // Superform
 import { BaseHook } from "../BaseHook.sol";
-import { ISuperHook, ISuperHookResult, ISuperHookContextAware } from "../../interfaces/ISuperHook.sol";
-import {
-    IPendleRouterV4,
-    ApproxParams,
-    TokenInput,
-    LimitOrderData,
-    TokenOutput,
-    FillOrderParams,
-    Order
-} from "../../../vendor/pendle/IPendleRouterV4.sol";
-import { IPendleMarket } from "../../../vendor/pendle/IPendleMarket.sol";
+import { BytesLib } from "../../../vendor/BytesLib.sol";
 import { HookSubTypes } from "../../libraries/HookSubTypes.sol";
 import { HookDataDecoder } from "../../libraries/HookDataDecoder.sol";
+import { ISuperHook, ISuperHookResult, ISuperHookContextAware } from "../../interfaces/ISuperHook.sol";
 
 /// @title PendleRouterRedeemHook
 /// @author Superform Labs
 /// @dev data has the following structure
-/// @notice         bytes4 placeholder = bytes4(BytesLib.slice(data, 0, 4), 0);
-/// @notice         address yieldSource = BytesLib.toAddress(data, 4);
-/// @notice         bool usePrevHookAmount = _decodeBool(data, 24);
-/// @notice         uint256 value = BytesLib.toUint256(data, 25);
-/// @notice         address receiver = BytesLib.toAddress(data, 57);
-/// @notice         address YT = BytesLib.toAddress(data, 89);
-/// @notice         address tokenOut = BytesLib.toAddress(data, 121);
-/// @notice         uint256 minTokenOut = BytesLib.toUint256(data, 153);
+/// @notice         uint256 amount = BytesLib.toUint256(data, 0);
+/// @notice         address receiver = BytesLib.toAddress(data, 32);
+/// @notice         address YT = BytesLib.toAddress(data, 52);
+/// @notice         address tokenOut = BytesLib.toAddress(data, 72);
+/// @notice         uint256 minTokenOut = BytesLib.toUint256(data, 92);
+/// @notice         bool usePrevHookAmount = _decodeBool(data, 124);
 contract PendleRouterRedeemHook is BaseHook, ISuperHookContextAware {
     using HookDataDecoder for bytes;
 
-    uint256 private constant USE_PREV_HOOK_AMOUNT_POSITION = 24;
-
-    /// @dev Creates a TokenOutput struct without using any swap aggregator
-    /// @param tokenOut must be one of the SY's tokens out (obtain via `IStandardizedYield#getTokensOut`)
-    /// @param minTokenOut minimum amount of token out
-    // function createTokenOutputSimple(address tokenOut, uint256 minTokenOut) pure returns (TokenOutput memory) {
-    //     return
-    //         TokenOutput({
-    //             tokenOut: tokenOut,
-    //             minTokenOut: minTokenOut,
-    //             tokenRedeemSy: tokenOut,
-    //             pendleSwap: address(0),
-    //             swapData: createSwapTypeNoAggregator()
-    //         });
-    // }
+    uint256 private constant USE_PREV_HOOK_AMOUNT_POSITION = 124;
 
     /*//////////////////////////////////////////////////////////////
                                  STORAGE
@@ -57,7 +33,6 @@ contract PendleRouterRedeemHook is BaseHook, ISuperHookContextAware {
     IPendleRouterV4 public immutable pendleRouterV4;
 
     struct RedeemData {
-        address yieldSource; // Pendle Market
         bool usePrevHookAmount;
         uint256 amount; // netPyIn
         address receiver;
@@ -70,9 +45,10 @@ contract PendleRouterRedeemHook is BaseHook, ISuperHookContextAware {
                                  ERRORS
     //////////////////////////////////////////////////////////////*/
     error YT_NOT_VALID();
-    error AMOUNT_NOT_VALID();
     error ORDER_NOT_MATURE();
     error RECEIVER_NOT_VALID();
+    error TOKEN_OUT_NOT_VALID();
+    error MIN_TOKEN_OUT_NOT_VALID();
 
     /*//////////////////////////////////////////////////////////////
                                CONSTRUCTOR
@@ -96,20 +72,20 @@ contract PendleRouterRedeemHook is BaseHook, ISuperHookContextAware {
         override
         returns (Execution[] memory executions)
     {
-        RedeemData memory redeemData = _decodeData(data);
-        _validateRedeemData(redeemData);
+        RedeemData memory redeemVars = _decodeData(data);
+        _validateRedeemData(redeemVars);
 
-        if (redeemData.usePrevHookAmount) {
-            vars.amount = ISuperHookResult(prevHook).outAmount();
+        if (redeemVars.usePrevHookAmount) {
+            redeemVars.amount = ISuperHookResult(prevHook).outAmount();
         }
 
-        TokenOutput memory output = pendleV4Router.createTokenOutputSimple(redeemData.tokenOut, redeemData.minTokenOut);
+        TokenOutput memory output = pendleRouterV4.createTokenOutputSimple(redeemVars.tokenOut, redeemVars.minTokenOut);
 
         executions = new Execution[](1);
         executions[0] = Execution({
             target: address(pendleRouterV4),
             value: 0,
-            callData: abi.encodeWithSelector(IPendleRouterV4.redeemPyToToken.selector, redeemData.receiver, redeemData.YT, redeemData.amount, output)
+            callData: abi.encodeWithSelector(IPendleRouterV4.redeemPyToToken.selector, redeemVars.receiver, redeemVars.YT, redeemVars.amount, output)
         });
     }
 
@@ -139,20 +115,21 @@ contract PendleRouterRedeemHook is BaseHook, ISuperHookContextAware {
         internal
         view
         returns (RedeemData memory redeemData)
-    {
-        address pendleMarket = data.extractYieldSource();
-        address receiver = data.extractReceiver();
-        address YT = data.extractYT();
-        uint256 amount = data.extractAmount();
-        address tokenOut = data.extractTokenOut();
-        uint256 minTokenOut = data.extractMinTokenOut();
+    {   
+        uint256 amount = BytesLib.toUint256(data, 0);
+        address receiver = BytesLib.toAddress(data, 32);
+        address YT = BytesLib.toAddress(data, 52);
+        address tokenOut = BytesLib.toAddress(data, 72);
+        uint256 minTokenOut = BytesLib.toUint256(data, 92);
+        bool usePrevHookAmount = _decodeBool(data, USE_PREV_HOOK_AMOUNT_POSITION);
 
-        RedeemData memory redeemData = RedeemData({
-            yieldSource: pendleMarket,
+        return RedeemData({
             usePrevHookAmount: usePrevHookAmount,
             amount: amount,
             receiver: receiver,
             YT: YT,
+            tokenOut: tokenOut,
+            minTokenOut: minTokenOut
         });
     }
 
