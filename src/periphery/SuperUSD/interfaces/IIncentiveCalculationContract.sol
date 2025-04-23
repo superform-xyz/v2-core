b6b11530270ff9460de3b5b90dbee4be6552cb19
Nicola Bernini <nicola.bernini@gmail.com>
2025-04-23 16:54:35 +0200

    Adding some interfaces

---
+++ src/periphery/SuperUSD/interfaces/IIncentiveCalculationContract.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+interface IIncentiveCalculationContract {
+    /**
+     * @notice Calculates the energy function.
+     * @param currentAllocation The allocation before the operation.
+     * @param allocationTarget The target allocation.
+     * @param weights The weights for each asset.
+     * @return res The calculated energy.
+     */
+    function energy(
+        uint256[] memory currentAllocation,
+        uint256[] memory allocationTarget,
+        uint256[] memory weights
+    ) external pure returns (uint256 res);
+
+    /**
+     * @notice Calculates the incentive.
+     * @param allocationPreOperation The allocation before the operation.
+     * @param allocationPostOperation The allocation after the operation.
+     * @param allocationTarget The target allocation.
+     * @param weights The weights for each asset.
+     * @param energyToTokenExchangeRatio The ratio for energy to token exchange.
+     * @return incentive The calculated incentive.
+     */
+    function calculateIncentive(
+        uint256[] memory allocationPreOperation,
+        uint256[] memory allocationPostOperation,
+        uint256[] memory allocationTarget,
+        uint256[] memory weights,
+        uint256 energyToTokenExchangeRatio
+    ) external view returns (int256 incentive);
+}
---
+++ src/periphery/SuperUSD/interfaces/IIncentiveFundContract.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+interface IIncentiveFundContract {
+    /**
+     * @notice Settles the incentive for a user.
+     * @param user The address of the user.
+     * @param amount The amount of incentive to settle (can be positive or negative).
+     */
+    function settleIncentive(address user, int256 amount) external;
+}
