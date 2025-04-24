59164a6f017e2d1c46a95a2f44f3e50a13fb5e6c
Tamara Ringas <tamriasdev@gmail.com>
2025-04-24 15:29:58 +0200

    feat: add build()

--- src/core/hooks/tokens/erc20/BatchTransferFromHook.sol
+++ src/core/hooks/tokens/erc20/BatchTransferFromHook.sol
@@ -28,4 +28,59 @@ contract BatchTransferFromHook is BaseHook, ISuperHookContextAware {
     /*//////////////////////////////////////////////////////////////
                                  VIEW METHODS
     //////////////////////////////////////////////////////////////*/
+    function build(
+        address prevHook,
+        address,
+        bytes memory data
+    )
+        external
+        view
+        override
+        returns (Execution[] memory executions)
+    {
+        address token = BytesLib.toAddress(data, 0);
+        address to = BytesLib.toAddress(data, 20);
+        uint256 amount = BytesLib.toUint256(data, 40);
+        bool usePrevHookAmount = _decodeBool(data, USE_PREV_HOOK_AMOUNT_POSITION);
+
+        if (usePrevHookAmount) {
+            amount = ISuperHookResult(prevHook).outAmount();
+        }
+
+        if (amount == 0) revert AMOUNT_NOT_VALID();
+        if (token == address(0)) revert ADDRESS_NOT_VALID();
+
+        // @dev no-revert-on-failure tokens are not supported
+        executions = new Execution[](1);
+        executions[0] = Execution({ target: token, value: 0, callData: abi.encodeCall(IERC20.transfer, (to, amount)) });
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                 EXTERNAL METHODS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @inheritdoc ISuperHookContextAware
+    function decodeUsePrevHookAmount(bytes memory data) external pure returns (bool) {
+        return _decodeBool(data, USE_PREV_HOOK_AMOUNT_POSITION);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                 INTERNAL METHODS
+    //////////////////////////////////////////////////////////////*/
+    function _preExecute(address, address, bytes calldata data) internal override {
+        outAmount = _getBalance(data);
+    }
+
+    function _postExecute(address, address, bytes calldata data) internal override {
+        outAmount = _getBalance(data) - outAmount;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                 PRIVATE METHODS
+    //////////////////////////////////////////////////////////////*/
+    function _getBalance(bytes memory data) private view returns (uint256) {
+        address token = BytesLib.toAddress(data, 0);
+        address to = BytesLib.toAddress(data, 20);
+        return IERC20(token).balanceOf(to);
+    }
 }
