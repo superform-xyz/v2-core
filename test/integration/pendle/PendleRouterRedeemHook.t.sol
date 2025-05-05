005f6adfe29b3ecf9f35897c7d1c0ef5568fb1a5
Tamara Ringas <tamriasdev@gmail.com>
2025-05-05 11:17:04 +0200

    feat: add test_PendleRouterRedeemHook()

--- test/integration/pendle/PendleRouterRedeemHook.t.sol
+++ test/integration/pendle/PendleRouterRedeemHook.t.sol
@@ -3,11 +3,9 @@ pragma solidity >=0.8.28;
 
 // Tests
 import { BaseTest } from "../../BaseTest.t.sol";
-import { console2 } from "forge-std/console2.sol";
 
 import { UserOpData, AccountInstance } from "modulekit/ModuleKit.sol";
-import { IPendleMarket } from "../../../src/vendor/pendle/IPendleMarket.sol";
-import { IStandardizedYield } from "../../../src/vendor/pendle/IStandardizedYield.sol";
+import { IPendleRouterV4, TokenInput, SwapData, SwapType } from "../../../src/vendor/pendle/IPendleRouterV4.sol";
 import { ISuperExecutor } from "../../../src/core/interfaces/ISuperExecutor.sol";
 import { IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
@@ -16,9 +14,11 @@ contract PendleRouterRedeemHookTest is BaseTest {
     AccountInstance public instance;
     address public account;
 
-    address public token;
+    IERC20 public eUSDe;
+    IERC20 public yt;
+    IERC20 public pt;
 
-    address public pendleMarket;
+    uint256 public constant expiry = 22_411_332;
 
     function setUp() public override {
         useLatestFork = true;
@@ -30,6 +30,63 @@ contract PendleRouterRedeemHookTest is BaseTest {
         instance = accountInstances[ETH];
         account = instance.account;
 
-        token = underlyingTokens[ETH][USDC_KEY];
+        // Token Out = Token Redeem Sy = Ethena USDe
+        eUSDe = IERC20(0x4c9EDD5852cd905f086C759E8383e09bff1E68B3);
+
+        // YT Ethena USDe
+        yt = IERC20(0x733Ee9Ba88f16023146EbC965b7A1Da18a322464);
+
+        // PT Ethena USDe
+        pt = IERC20(0x917459337CaAC939D41d7493B3999f571D20D667);
+
+        deal(address(eUSDe), account, 10e18);
+    }
+
+    function test_PendleRouterRedeemHook() public {
+        vm.warp(22_384_742);
+
+        uint256 eUSDeBalance = eUSDe.balanceOf(account);
+        uint256 ptBalance = pt.balanceOf(account);
+
+        TokenInput memory tokenInput = TokenInput({
+            tokenIn: address(eUSDe),
+            netTokenIn: 1e18,
+            tokenMintSy: address(eUSDe),
+            pendleSwap: address(0),
+            swapData: SwapData({ swapType: SwapType.NONE, extRouter: address(0), extCalldata: bytes(""), needScale: false })
+        });
+
+        vm.startPrank(account);
+        eUSDe.approve(address(IPendleRouterV4(PENDLE_ROUTERS[ETH])), 1e18);
+        IPendleRouterV4(PENDLE_ROUTERS[ETH]).mintPyFromToken(
+            account, // receiver
+            address(yt), // YT
+            0.7e18, // minPyOut
+            tokenInput
+        );
+
+        assertEq(eUSDe.balanceOf(account), eUSDeBalance - 1e18);
+        assertGt(pt.balanceOf(account), ptBalance);
+
+        vm.warp(expiry + 1 days);
+
+        address redeemHook = _getHookAddress(ETH, PENDLE_ROUTER_REDEEM_HOOK_KEY);
+
+        address[] memory hooks = new address[](1);
+        hooks[0] = redeemHook;
+
+        bytes[] memory data = new bytes[](1);
+        data[0] =
+            _createPendleRedeemHookData(1e18, address(yt), address(pt), address(eUSDe), address(eUSDe), 1e17, false);
+
+        ISuperExecutor.ExecutorEntry memory entry =
+            ISuperExecutor.ExecutorEntry({ hooksAddresses: hooks, hooksData: data });
+
+        UserOpData memory userOpData = _getExecOps(instance, superExecutor, abi.encode(entry));
+
+        executeOp(userOpData);
+
+        assertEq(eUSDe.balanceOf(account), eUSDeBalance);
+        assertEq(pt.balanceOf(account), ptBalance);
     }
 }
