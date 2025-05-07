07cd4c72ecc38ef735bae7b725e3a48888d8c4ab
Nicola Bernini <nicola.bernini@gmail.com>
2025-05-07 11:40:12 +0200

    Placing MockAggregator.sol in its own contract

--- test/periphery/SuperAsset/IncentiveFundContract.t.sol
+++ test/periphery/SuperAsset/IncentiveFundContract.t.sol
@@ -13,55 +13,7 @@ import {IncentiveCalculationContract} from "../../../src/periphery/SuperAsset/In
 import {MockERC20} from "../../mocks/MockERC20.sol";
 import {IAccessControl} from "@openzeppelin/contracts/access/IAccessControl.sol";
 import {IERC20Errors} from "@openzeppelin/contracts/interfaces/draft-IERC6093.sol";
-import {AggregatorV3Interface} from "../../../src/vendor/chainlink/AggregatorV3Interface.sol";
-
-contract MockAggregator is AggregatorV3Interface {
-    int256 private _answer;
-    uint256 private _updatedAt;
-    uint8 private immutable _decimals;
-
-    constructor(int256 answer_, uint8 decimals_) {
-        _answer = answer_;
-        _decimals = decimals_;
-        _updatedAt = block.timestamp;
-    }
-
-    function setAnswer(int256 answer_) external {
-        _answer = answer_;
-    }
-
-    function setUpdatedAt(uint256 updatedAt_) external {
-        _updatedAt = updatedAt_;
-    }
-
-    function decimals() external view returns (uint8) {
-        return _decimals;
-    }
-
-    function description() external pure returns (string memory) {
-        return "Mock Aggregator";
-    }
-
-    function version() external pure returns (uint256) {
-        return 1;
-    }
-
-    function getRoundData(uint80)
-        external
-        view
-        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
-    {
-        return (1, _answer, block.timestamp, _updatedAt, 1);
-    }
-
-    function latestRoundData()
-        external
-        view
-        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
-    {
-        return (1, _answer, block.timestamp, _updatedAt, 1);
-    }
-}
+import {MockAggregator} from "./MockAggregator.sol";
 
 contract IncentiveFundContractTest is Test {
     // --- Events ---
