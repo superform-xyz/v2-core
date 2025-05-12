3ce597aba8d4797a995d3db50e4f5062f44aab08
Tamara Ringas <tamriasdev@gmail.com>
2025-05-12 15:01:33 +0200

    fix: rm unused var

--- test/unit/hooks/vaults/4626/ERC4626VaultHooksTest.t.sol
+++ test/unit/hooks/vaults/4626/ERC4626VaultHooksTest.t.sol
@@ -155,9 +155,6 @@ contract ERC4626VaultHooksTest is Helpers {
   }
 
   function test_RedeemHook_ZeroAddress() public {
-    address _yieldSource = yieldSource;
-
-    yieldSource = address(0);
     vm.expectRevert(BaseHook.ADDRESS_NOT_VALID.selector);
     redeemHook.build(address(0), address(this), abi.encodePacked(yieldSourceOracleId, yieldSource, address(this), shares, false));
   }
