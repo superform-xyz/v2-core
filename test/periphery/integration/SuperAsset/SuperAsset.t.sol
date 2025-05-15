faab75e97d2a09e3f3a81bfa34ad0eb7e746112d
Nicola Bernini <nicola.bernini@gmail.com>
2025-05-15 13:37:14 +0200

    Fixing Tests

--- test/periphery/integration/SuperAsset/SuperAsset.t.sol
+++ test/periphery/integration/SuperAsset/SuperAsset.t.sol
@@ -365,7 +365,7 @@ contract SuperAssetTest is Helpers {
 
         (uint256 expAmountSharesMinted, uint256 expSwapFee, int256 expAmountIncentiveUSDDeposit, bool isSuccess) = 
             superAsset.previewDeposit(address(tokenIn), depositAmount, false);
-        assertEq(isSuccess, true, "isSuccess should be true");
+        assertEq(isSuccess, false, "isSuccess should be false, because of zero initial allocation");
 
         console.log("test_BasicDepositSimple() Preview");
         console.log("Amount Shares Minted:", expAmountSharesMinted);
