e43abf2db7e99588f6be6bc185a86a4e9acd943e
Tamara Ringas <tamriasdev@gmail.com>
2025-05-21 15:46:45 +0200

    feat: add toggle for mock executions

--- test/integration/MinimalBaseIntegrationTest.t.sol
+++ test/integration/MinimalBaseIntegrationTest.t.sol
@@ -1,27 +1,27 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.30;
+pragma solidity >=0.8.30;
 
 // external
-import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 
 // Superform
-import {ISuperExecutor} from "../../src/core/interfaces/ISuperExecutor.sol";
-import {Helpers} from "../utils/Helpers.sol";
-import {InternalHelpers} from "../utils/InternalHelpers.sol";
-import {RhinestoneModuleKit, ModuleKitHelpers, AccountInstance} from "modulekit/ModuleKit.sol";
-import {MODULE_TYPE_EXECUTOR} from "modulekit/accounts/kernel/types/Constants.sol";
-import {ERC4626YieldSourceOracle} from "../../src/core/accounting/oracles/ERC4626YieldSourceOracle.sol";
-import {ERC5115YieldSourceOracle} from "../../src/core/accounting/oracles/ERC5115YieldSourceOracle.sol";
-import {ERC7540YieldSourceOracle} from "../../src/core/accounting/oracles/ERC7540YieldSourceOracle.sol";
-import {SuperLedgerConfiguration} from "../../src/core/accounting/SuperLedgerConfiguration.sol";
-import {SuperExecutor} from "../../src/core/executors/SuperExecutor.sol";
-import {SuperLedger} from "../../src/core/accounting/SuperLedger.sol";
-import {ERC5115Ledger} from "../../src/core/accounting/ERC5115Ledger.sol";
-import {ISuperLedgerConfiguration} from "../../src/core/interfaces/accounting/ISuperLedgerConfiguration.sol";
-import {ISuperLedger} from "../../src/core/interfaces/accounting/ISuperLedger.sol";
-import {ApproveERC20Hook} from "../../src/core/hooks/tokens/erc20/ApproveERC20Hook.sol";
-import {Deposit4626VaultHook} from "../../src/core/hooks/vaults/4626/Deposit4626VaultHook.sol";
-import {Redeem4626VaultHook} from "../../src/core/hooks/vaults/4626/Redeem4626VaultHook.sol";
+import { ISuperExecutor } from "../../src/core/interfaces/ISuperExecutor.sol";
+import { Helpers } from "../utils/Helpers.sol";
+import { InternalHelpers } from "../utils/InternalHelpers.sol";
+import { RhinestoneModuleKit, ModuleKitHelpers, AccountInstance } from "modulekit/ModuleKit.sol";
+import { MODULE_TYPE_EXECUTOR } from "modulekit/accounts/kernel/types/Constants.sol";
+import { ERC4626YieldSourceOracle } from "../../src/core/accounting/oracles/ERC4626YieldSourceOracle.sol";
+import { ERC5115YieldSourceOracle } from "../../src/core/accounting/oracles/ERC5115YieldSourceOracle.sol";
+import { ERC7540YieldSourceOracle } from "../../src/core/accounting/oracles/ERC7540YieldSourceOracle.sol";
+import { SuperLedgerConfiguration } from "../../src/core/accounting/SuperLedgerConfiguration.sol";
+import { SuperExecutor } from "../../src/core/executors/SuperExecutor.sol";
+import { SuperLedger } from "../../src/core/accounting/SuperLedger.sol";
+import { ERC5115Ledger } from "../../src/core/accounting/ERC5115Ledger.sol";
+import { ISuperLedgerConfiguration } from "../../src/core/interfaces/accounting/ISuperLedgerConfiguration.sol";
+import { ISuperLedger } from "../../src/core/interfaces/accounting/ISuperLedger.sol";
+import { ApproveERC20Hook } from "../../src/core/hooks/tokens/erc20/ApproveERC20Hook.sol";
+import { Deposit4626VaultHook } from "../../src/core/hooks/vaults/4626/Deposit4626VaultHook.sol";
+import { Redeem4626VaultHook } from "../../src/core/hooks/vaults/4626/Redeem4626VaultHook.sol";
 import "forge-std/console2.sol";
 
 /// @dev Forked mainnet test with deposit and redeem flow for a real ERC4626 vault
@@ -44,6 +44,8 @@ abstract contract MinimalBaseIntegrationTest is Helpers, RhinestoneModuleKit, In
     address public redeem4626Hook;
     uint256 public blockNumber;
 
+    bool public useRealOdosRouter;
+
     function setUp() public virtual {
         blockNumber != 0
             ? vm.createSelectFork(vm.envString(ETHEREUM_RPC_URL_KEY), blockNumber)
@@ -61,7 +63,7 @@ abstract contract MinimalBaseIntegrationTest is Helpers, RhinestoneModuleKit, In
         ledgerConfig = ISuperLedgerConfiguration(address(new SuperLedgerConfiguration()));
 
         superExecutorOnEth = ISuperExecutor(new SuperExecutor(address(ledgerConfig)));
-        instanceOnEth.installModule({moduleTypeId: MODULE_TYPE_EXECUTOR, module: address(superExecutorOnEth), data: ""});
+        instanceOnEth.installModule({ moduleTypeId: MODULE_TYPE_EXECUTOR, module: address(superExecutorOnEth), data: "" });
 
         address[] memory allowedExecutors = new address[](1);
         allowedExecutors[0] = address(superExecutorOnEth);
@@ -96,5 +98,11 @@ abstract contract MinimalBaseIntegrationTest is Helpers, RhinestoneModuleKit, In
         approveHook = address(new ApproveERC20Hook());
         deposit4626Hook = address(new Deposit4626VaultHook());
         redeem4626Hook = address(new Redeem4626VaultHook());
+
+        useRealOdosRouter = false;
+    }
+
+    function _toggleUseRealOdosRouter(bool _useRealOdosRouter) public {
+        useRealOdosRouter = _useRealOdosRouter;
     }
 }
