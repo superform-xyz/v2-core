746d29fe74c1210ea9bbf88a847508b7e6248b38
subhasishgoswami <subhasishgoswami00@gmail.com>
2025-06-11 10:05:32 +0530

    feat: support native token in batchTransferERC20Hook

--- src/core/hooks/tokens/erc20/BatchTransferERC20Hook.sol
+++ src/core/hooks/tokens/erc20/BatchTransferERC20Hook.sol
@@ -11,6 +11,8 @@ import {BaseHook} from "../../BaseHook.sol";
 import {HookSubTypes} from "../../../libraries/HookSubTypes.sol";
 import {ISuperHookInspector} from "../../../interfaces/ISuperHook.sol";
 
+address constant NATIVE_TOKEN = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
 /// @title BatchTransferERC20Hook
 /// @author Superform Labs
 /// @dev data has the following structure
@@ -27,23 +29,39 @@ contract BatchTransferERC20Hook is BaseHook, ISuperHookInspector {
                                  VIEW METHODS
     //////////////////////////////////////////////////////////////*/
 
-    function _buildHookExecutions(address, address, bytes calldata data)
-        internal
-        pure
-        override
-        returns (Execution[] memory executions)
-    {
+    function _buildHookExecutions(
+        address,
+        address,
+        bytes calldata data
+    ) internal pure override returns (Execution[] memory executions) {
         address to = BytesLib.toAddress(data, 0);
         bytes memory tokensData = BytesLib.slice(data, 20, data.length - 20);
 
-        (address[] memory tokens, uint256[] memory amounts) = abi.decode(tokensData, (address[], uint256[]));
+        (address[] memory tokens, uint256[] memory amounts) = abi.decode(
+            tokensData,
+            (address[], uint256[])
+        );
 
         uint256 tokensLen = tokens.length;
         if (tokensLen != amounts.length) revert LENGTH_MISMATCH();
 
         executions = new Execution[](tokensLen);
         for (uint i; i < tokensLen; i++) {
-            executions[i] = Execution({target: tokens[i], value: 0, callData: abi.encodeCall(IERC20.transfer, (to, amounts[i]))});
+            if (tokens[i] == NATIVE_TOKEN) {
+                // For native token, send ETH directly to the recipient
+                executions[i] = Execution({
+                    target: to,
+                    value: amounts[i],
+                    callData: ""
+                });
+            } else {
+                // For ERC20 tokens, use the standard transfer
+                executions[i] = Execution({
+                    target: tokens[i],
+                    value: 0,
+                    callData: abi.encodeCall(IERC20.transfer, (to, amounts[i]))
+                });
+            }
         }
     }
 
@@ -52,25 +70,39 @@ contract BatchTransferERC20Hook is BaseHook, ISuperHookInspector {
     //////////////////////////////////////////////////////////////*/
     /// @inheritdoc ISuperHookInspector
     function inspect(bytes calldata data) external pure returns (bytes memory) {
-        bytes memory tokensData = BytesLib.slice(data, 53, data.length - 53);
-        (address[] memory tokens,) = abi.decode(tokensData, (address[], uint256[]));
+        // First 20 bytes is the 'to' address
+        address to = BytesLib.toAddress(data, 0);
         
-        bytes memory result = abi.encodePacked(
-            BytesLib.toAddress(data, 0) //to
+        // The rest is abi encoded (address[] tokens, uint256[] amounts)
+        bytes memory tokensData = BytesLib.slice(data, 20, data.length - 20);
+        (address[] memory tokens, ) = abi.decode(
+            tokensData,
+            (address[], uint256[])
         );
 
+        // Return the 'to' address and all token addresses
+        bytes memory result = abi.encodePacked(to);
+        
         uint256 tokensLen = tokens.length;
         for (uint i; i < tokensLen; i++) {
             result = abi.encodePacked(result, tokens[i]);
         }
-
+        
         return result;
     }
 
     /*//////////////////////////////////////////////////////////////
                                  INTERNAL METHODS
     //////////////////////////////////////////////////////////////*/
-    function _preExecute(address, address, bytes calldata data) internal override {}
+    function _preExecute(
+        address,
+        address,
+        bytes calldata data
+    ) internal override {}
 
-    function _postExecute(address, address, bytes calldata data) internal override {}
+    function _postExecute(
+        address,
+        address,
+        bytes calldata data
+    ) internal override {}
 }
