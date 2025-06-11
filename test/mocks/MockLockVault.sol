ead3e9e69a12703f909511845533c6cbf98c9d76
Tamara Ringas <tamriasdev@gmail.com>
2025-06-11 10:59:23 +0200

    fix: rm compilation warning

--- test/mocks/MockLockVault.sol
+++ test/mocks/MockLockVault.sol
@@ -4,7 +4,11 @@ pragma solidity 0.8.30;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockLockVault {
+    error INVALID_HOOK();
+
     function lock(address account, address token, address hook, uint256 amount) external {
+        if (hook == address(0)) revert INVALID_HOOK();
+
         ERC20(token).transferFrom(account, address(this), amount);
     }
 
