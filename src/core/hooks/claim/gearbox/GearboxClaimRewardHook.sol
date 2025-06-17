b9ce626748c8d4bcfaf79a325ab7b03cd05fa27d
Cosmin <cosmin@superform.xyz>
2025-06-17 13:21:57 +0300

    feat: [SUP-12276] audit cantina public issue 17 (#497)
    Co-authored-by: 0xTimepunk <jldcmartins@gmail.com>

--- src/core/hooks/claim/gearbox/GearboxClaimRewardHook.sol
+++ src/core/hooks/claim/gearbox/GearboxClaimRewardHook.sol
@@ -77,14 +77,14 @@ contract GearboxClaimRewardHook is
     /*//////////////////////////////////////////////////////////////
                                  INTERNAL METHODS
     //////////////////////////////////////////////////////////////*/
-    function _preExecute(address, address, bytes calldata data) internal override {
+    function _preExecute(address, address account, bytes calldata data) internal override {
         asset = BytesLib.toAddress(data, 24);
         if (asset == address(0)) revert ASSET_ZERO_ADDRESS();
 
-        outAmount = _getBalance(data);
+        outAmount = _getBalance(data, account);
     }
 
-    function _postExecute(address, address, bytes calldata data) internal override {
-        outAmount = _getBalance(data) - outAmount;
+    function _postExecute(address, address account, bytes calldata data) internal override {
+        outAmount = _getBalance(data, account) - outAmount;
     }
 }
