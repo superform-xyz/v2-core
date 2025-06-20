02d4e9f0458fdc7f154aa09476ec9e7b789f3506
supervaulter <cosmin@superform.xyz>
2025-06-20 10:50:06 +0300

    chore: added poc

--- test/integration/ERC4626DepositRedeemFlow.t.sol
+++ test/integration/ERC4626DepositRedeemFlow.t.sol
@@ -24,6 +24,65 @@ contract ERC4626DepositRedeemFlowTest is MinimalBaseIntegrationTest {
         super.setUp();
     }
 
+    function test_failsToRedeemFullBalance() public {
+        uint256 amount = 1e8;
+        address[] memory hooksAddresses = new address[](2);
+        hooksAddresses[0] = approveHook;
+        hooksAddresses[1] = deposit4626Hook;
+        bytes[] memory hooksData = new bytes[](2);
+        hooksData[0] = _createApproveHookData(underlyingEth_USDC, yieldSourceAddressEth, amount, false);
+        hooksData[1] = _createDeposit4626HookData(
+            bytes4(bytes(ERC4626_YIELD_SOURCE_ORACLE_KEY)), yieldSourceAddressEth, amount, false, address(0), 0
+        );
+
+        ISuperExecutor.ExecutorEntry memory entry =
+            ISuperExecutor.ExecutorEntry({hooksAddresses: hooksAddresses, hooksData: hooksData});
+
+        UserOpData memory userOpData = _getExecOps(instanceOnEth, superExecutorOnEth, abi.encode(entry));
+        vm.expectEmit(true, true, true, false);
+        emit ISuperLedgerData.AccountingInflow(accountEth, yieldSourceOracle, yieldSourceAddressEth, amount, 1e18);
+        executeOp(userOpData);
+
+        // user transfers some shares from other account to op's sender
+        deal(address(vaultInstanceEth), address(this), 10);
+        IERC4626(vaultInstanceEth).transfer(accountEth, 10);
+
+        uint256 accSharesAfter = vaultInstanceEth.balanceOf(accountEth);
+
+        hooksAddresses = new address[](1);
+        hooksAddresses[0] = redeem4626Hook;
+        hooksData = new bytes[](1);
+        hooksData[0] = _createRedeem4626HookData(
+            bytes4(bytes(ERC4626_YIELD_SOURCE_ORACLE_KEY)),
+            yieldSourceAddressEth,
+            accountEth,
+            accSharesAfter,
+            false
+        );
+
+        ISuperExecutor.ExecutorEntry memory entryWithdraw =
+            ISuperExecutor.ExecutorEntry({hooksAddresses: hooksAddresses, hooksData: hooksData});
+
+        userOpData = _getExecOps(instanceOnEth, superExecutorOnEth, abi.encode(entryWithdraw));
+
+        vm.expectEmit(true, true, true, false);
+        emit ISuperLedgerData.AccountingOutflow(accountEth, yieldSourceOracle, yieldSourceAddressEth, accSharesAfter, 0);
+
+        executeOp(userOpData);
+        // ^ does not revert anymore
+        
+        // ASSERTIONS
+        
+        uint256 finalShareBalance = vaultInstanceEth.balanceOf(accountEth);
+        assertEq(finalShareBalance, 0);
+        
+        uint256 expectedTokensReturned = IERC4626(yieldSourceAddressEth).convertToAssets(accSharesAfter);
+        uint256 actualTokenBalance = IERC20(underlyingEth_USDC).balanceOf(accountEth);
+        assertGt(actualTokenBalance, 0);
+        
+
+    }
+
     function test_Deposit_4626_Mainnet_Flow() public {
         uint256 amount = 1e8;
         address[] memory hooksAddresses = new address[](2);
