7142ee5575db1988d05e708773ec043cbb72faef
Tamara Ringas <tamriasdev@gmail.com>
2025-06-20 10:25:37 +0200

    fix: use supply and borrow hook

--- test/integration/MorphoBorrow.t.sol
+++ test/integration/MorphoBorrow.t.sol
@@ -1,14 +1,15 @@
-import {MinimalBaseIntegrationTest} from "./MinimalBaseIntegrationTest.t.sol";
-import {MorphoBorrowHook} from "../../src/core/hooks/loan/morpho/MorphoBorrowHook.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {ISuperExecutor} from "../../src/core/interfaces/ISuperExecutor.sol";
-import {UserOpData} from "modulekit/ModuleKit.sol";
-import {console} from "forge-std/console.sol";
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.30;
 
+import { MinimalBaseIntegrationTest } from "./MinimalBaseIntegrationTest.t.sol";
+import { MorphoSupplyAndBorrowHook } from "../../src/core/hooks/loan/morpho/MorphoSupplyAndBorrowHook.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { ISuperExecutor } from "../../src/core/interfaces/ISuperExecutor.sol";
+import { UserOpData } from "modulekit/ModuleKit.sol";
+import { console } from "forge-std/console.sol";
 
-contract MorphoBorrowTest is MinimalBaseIntegrationTest {
-    address public morphoBorrowHook;
-    address public morphoRepayHook;
+contract MorphoSupplyAndBorrowTest is MinimalBaseIntegrationTest {
+    address public morphoSupplyAndBorrowHook;
 
     address public constant CHAIN_1_WBTC = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
 
@@ -19,14 +20,13 @@ contract MorphoBorrowTest is MinimalBaseIntegrationTest {
     uint256 public lltv;
     uint256 public lltvRatio;
 
-
     function setUp() public override {
         blockNumber = ETH_BLOCK;
         super.setUp();
 
-        morphoBorrowHook = address(new MorphoBorrowHook(address(MORPHO)));
+        morphoSupplyAndBorrowHook = address(new MorphoSupplyAndBorrowHook(address(MORPHO)));
 
-        amount = 1000000;
+        amount = 1_000_000;
         lltv = 860_000_000_000_000_000;
         lltvRatio = 660_000_000_000_000_000;
 
@@ -42,31 +42,24 @@ contract MorphoBorrowTest is MinimalBaseIntegrationTest {
         // Log initial balances
         uint256 collateralBefore = IERC20(collateralToken).balanceOf(accountEth);
         uint256 loanBefore = IERC20(loanToken).balanceOf(accountEth);
-        
+
         console.log("Initial Balances:");
         console.log("  Collateral (WBTC):", collateralBefore);
         console.log("  Loan Token (USDC):", loanBefore);
-        
+
         // Setup the borrow hook execution
         address[] memory hooksAddresses = new address[](1);
-        hooksAddresses[0] = morphoBorrowHook;
+        hooksAddresses[0] = morphoSupplyAndBorrowHook;
 
         bytes[] memory hooksData = new bytes[](1);
-        hooksData[0] = _createMorphoBorrowHookData(
-            loanToken, 
-            collateralToken, 
-            morpho_oracle_wbtc_usdc, 
-            morpho_irm_wbtc_usdc, 
-            amount, 
-            lltvRatio, 
-            false, 
-            lltv
+        hooksData[0] = _createMorphoSupplyAndBorrowHookData(
+            loanToken, collateralToken, morpho_oracle_wbtc_usdc, morpho_irm_wbtc_usdc, amount, lltvRatio, false, lltv
         );
 
         ISuperExecutor.ExecutorEntry memory entry =
-            ISuperExecutor.ExecutorEntry({hooksAddresses: hooksAddresses, hooksData: hooksData});
+            ISuperExecutor.ExecutorEntry({ hooksAddresses: hooksAddresses, hooksData: hooksData });
         UserOpData memory userOpData = _getExecOps(instanceOnEth, superExecutorOnEth, abi.encode(entry));
-        
+
         // Execute the borrow operation
         executeOp(userOpData);
 
