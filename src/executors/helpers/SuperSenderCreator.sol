45b84e8e1614076c29e6a55a2aea707b96d4681d
supervaulter <cosmin@superform.xyz>
2025-07-09 14:59:01 +0300

    patch: added SuperSenderCreator contract

---
+++ src/executors/helpers/SuperSenderCreator.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.30;
+
+
+/// @title SuperSenderCreator
+/// @author Superform Labs
+/// @notice Contract to create sender accounts from initCode
+/// @dev This contract is used by SuperDestinationExecutor to create sender accounts
+contract SuperSenderCreator {
+     /**
+     * call the "initCode" factory to create and return the sender account address
+     * @param initCode the initCode value from a UserOp. contains 20 bytes of factory address, followed by calldata
+     * @return sender the returned address of the created account, or zero address on failure.
+     */
+    function createSender(bytes calldata initCode) external returns (address sender) {
+        address initAddress = address(bytes20(initCode[0 : 20]));
+        bytes memory initCallData = initCode[20 :];
+        bool success;
+        /* solhint-disable no-inline-assembly */
+        assembly {
+            success := call(gas(), initAddress, 0, add(initCallData, 0x20), mload(initCallData), 0, 32)
+            sender := mload(0)
+        }
+        if (!success) {
+            sender = address(0);
+        }
+    }
+}
--- test/BaseTest.t.sol
+++ test/BaseTest.t.sol
@@ -15,6 +15,7 @@ import { ISuperLedger } from "../src/interfaces/accounting/ISuperLedger.sol";
 import { ISuperLedgerConfiguration } from "../src/interfaces/accounting/ISuperLedgerConfiguration.sol";
 import { ISuperDestinationExecutor } from "../src/interfaces/ISuperDestinationExecutor.sol";
 import { ISuperValidator } from "../src/interfaces/ISuperValidator.sol";
+import { ISuperSenderCreator } from "../src/interfaces/ISuperSenderCreator.sol";
 
 // Superform contracts coded
 import { SuperLedger } from "../src/accounting/SuperLedger.sol";
@@ -22,6 +23,7 @@ import { ERC5115Ledger } from "./mocks/ERC5115Ledger.sol";
 import { SuperLedgerConfiguration } from "../src/accounting/SuperLedgerConfiguration.sol";
 import { SuperExecutor } from "../src/executors/SuperExecutor.sol";
 import { SuperDestinationExecutor } from "../src/executors/SuperDestinationExecutor.sol";
+import { SuperSenderCreator } from "../src/executors/helpers/SuperSenderCreator.sol";
 import { SuperMerkleValidator } from "../src/validators/SuperMerkleValidator.sol";
 import { SuperDestinationValidator } from "../src/validators/SuperDestinationValidator.sol";
 import { SuperValidatorBase } from "../src/validators/SuperValidatorBase.sol";
@@ -167,6 +169,7 @@ struct Addresses {
     ISuperRegistry superRegistry;
     ISuperExecutor superExecutor;
     ISuperExecutor superDestinationExecutor;
+    SuperSenderCreator superSenderCreator;
     AcrossV3Adapter acrossV3Adapter;
     DebridgeAdapter debridgeAdapter;
     ApproveERC20Hook approveErc20Hook;
@@ -497,6 +500,10 @@ contract BaseTest is Helpers, RhinestoneModuleKit, SignatureHelper, MerkleTreeHe
             );
             vm.label(address(A[i].superDestinationExecutor), SUPER_DESTINATION_EXECUTOR_KEY);
             contractAddresses[chainIds[i]][SUPER_DESTINATION_EXECUTOR_KEY] = address(A[i].superDestinationExecutor);
+
+            A[i].superSenderCreator = new SuperSenderCreator{ salt: SALT }();
+            vm.label(address(A[i].superSenderCreator), SUPER_SENDER_CREATOR_KEY);
+            contractAddresses[chainIds[i]][SUPER_SENDER_CREATOR_KEY] = address(A[i].superSenderCreator);
 
             A[i].acrossV3Adapter = new AcrossV3Adapter{ salt: SALT }(
                 SPOKE_POOL_V3_ADDRESSES[chainIds[i]], address(A[i].superDestinationExecutor)
@@ -1653,7 +1660,14 @@ contract BaseTest is Helpers, RhinestoneModuleKit, SignatureHelper, MerkleTreeHe
         returns (address)
     {
         (, address account) = _createAccountCreationData_DestinationExecutor(
-            validator, signer, _getContract(chainId, SUPER_DESTINATION_EXECUTOR_KEY), nexusFactory, nexusBootstrap
+            AccountCreationParams({
+                senderCreatorOnDestinationChain: _getContract(chainId, SUPER_SENDER_CREATOR_KEY),
+                validatorOnDestinationChain: validator,
+                theSigner: signer,
+                executorOnDestinationChain: _getContract(chainId, SUPER_DESTINATION_EXECUTOR_KEY),
+                nexusFactory: nexusFactory,
+                nexusBootstrap: nexusBootstrap
+            })
         );
         return account;
     }
@@ -1669,11 +1683,14 @@ contract BaseTest is Helpers, RhinestoneModuleKit, SignatureHelper, MerkleTreeHe
         bytes memory accountCreationData;
         if (messageData.account == address(0)) {
             (accountCreationData, accountToUse) = _createAccountCreationData_DestinationExecutor(
-                messageData.validator,
-                messageData.signer,
-                _getContract(messageData.chainId, SUPER_DESTINATION_EXECUTOR_KEY),
-                messageData.nexusFactory,
-                messageData.nexusBootstrap
+                AccountCreationParams({
+                    senderCreatorOnDestinationChain: _getContract(messageData.chainId, SUPER_SENDER_CREATOR_KEY),
+                    validatorOnDestinationChain: messageData.validator,
+                    theSigner: messageData.signer,
+                    executorOnDestinationChain: _getContract(messageData.chainId, SUPER_DESTINATION_EXECUTOR_KEY),
+                    nexusFactory: messageData.nexusFactory,
+                    nexusBootstrap: messageData.nexusBootstrap
+                })
             );
             messageData.account = accountToUse; // prefill the account to use
         } else {
@@ -1793,39 +1810,49 @@ contract BaseTest is Helpers, RhinestoneModuleKit, SignatureHelper, MerkleTreeHe
         return abi.encodeWithSelector(ISuperExecutor.execute.selector, abi.encode(entryToExecute));
     }
 
+    struct AccountCreationParams {
+        address senderCreatorOnDestinationChain;
+        address validatorOnDestinationChain;
+        address theSigner;
+        address executorOnDestinationChain;
+        address nexusFactory;
+        address nexusBootstrap;
+    }
     function _createAccountCreationData_DestinationExecutor(
-        address validatorOnDestinationChain,
-        address theSigner,
-        address executorOnDestinationChain,
-        address nexusFactory,
-        address nexusBootstrap
+        AccountCreationParams memory p
     )
         internal
         returns (bytes memory, address)
     {
         // create validators
         BootstrapConfig[] memory validators = new BootstrapConfig[](1);
-        validators[0] = BootstrapConfig({ module: validatorOnDestinationChain, data: abi.encode(theSigner) });
+        validators[0] = BootstrapConfig({ module: p.validatorOnDestinationChain, data: abi.encode(p.theSigner) });
+
         // create executors
         BootstrapConfig[] memory executors = new BootstrapConfig[](1);
-        executors[0] = BootstrapConfig({ module: address(executorOnDestinationChain), data: "" });
+        executors[0] = BootstrapConfig({ module: p.executorOnDestinationChain, data: "" });
+
         // create hooks
         BootstrapConfig memory hook = BootstrapConfig({ module: address(0), data: "" });
+
         // create fallbacks
         BootstrapConfig[] memory fallbacks = new BootstrapConfig[](0);
+
         address[] memory attesters = new address[](1);
         attesters[0] = address(MANAGER);
         uint8 threshold = 1;
+
         MockRegistry nexusRegistry = new MockRegistry();
-        bytes memory initData = INexusBootstrap(nexusBootstrap).getInitNexusCalldata(
+        bytes memory initData = INexusBootstrap(p.nexusBootstrap).getInitNexusCalldata(
             validators, executors, hook, fallbacks, IERC7484(nexusRegistry), attesters, threshold
         );
+
         bytes32 initSalt = bytes32(keccak256("SIGNER_SALT"));
 
-        address precomputedAddress = INexusFactory(nexusFactory).computeAccountAddress(initData, initSalt);
-
+        address precomputedAddress = INexusFactory(p.nexusFactory).computeAccountAddress(initData, initSalt);
         bytes memory initFactoryCalldata = abi.encodeWithSelector(INexusFactory.createAccount.selector, initData, initSalt);
-        return (abi.encodePacked(address(nexusFactory), initFactoryCalldata), precomputedAddress);
+
+    return (abi.encodePacked(p.senderCreatorOnDestinationChain, address(p.nexusFactory), initFactoryCalldata), precomputedAddress);
     }
 
     function _createAcrossV3ReceiveFundsAndExecuteHookData( 
