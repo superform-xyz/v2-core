b957cacb278c5aac87128a74cd78b6889a5ce99b
Cosmin <cosmin@superform.xyz>
2025-08-20 21:21:17 +0300

    patch: slippage library update (#780)

--- src/libraries/HookDataUpdater.sol
+++ src/libraries/HookDataUpdater.sol
@@ -7,12 +7,13 @@ library HookDataUpdater {
     uint256 private constant PRECISION = 1e5;
 
     function getUpdatedOutputAmount(uint256 amount, uint256 _prevAmount, uint256 outputAmount) internal pure returns (uint256) {
+        if (_prevAmount == 0) return outputAmount;
         if (amount != _prevAmount) {
             if (amount > _prevAmount) {
-                uint256 percentIncrease = _prevAmount > 0 ? Math.mulDiv(amount - _prevAmount, PRECISION, _prevAmount) : PRECISION;
+                uint256 percentIncrease = Math.mulDiv(amount - _prevAmount, PRECISION, _prevAmount);
                 outputAmount = outputAmount + Math.mulDiv(outputAmount, percentIncrease, PRECISION);
             } else {
-                uint256 percentDecrease = _prevAmount > 0 ? Math.mulDiv(_prevAmount - amount, PRECISION, _prevAmount) : PRECISION;
+                uint256 percentDecrease = Math.mulDiv(_prevAmount - amount, PRECISION, _prevAmount);
                 uint256 decreaseAmount = Math.mulDiv(outputAmount, percentDecrease, PRECISION);
                 if (decreaseAmount > outputAmount) {
                     outputAmount = 0;
