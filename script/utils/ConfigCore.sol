f9d600bce02f1fd59710b23b3fa624711d5c9ee7
supervaulter <cosmin@superform.xyz>
2026-01-28 19:15:21 +0200

    chore: deploy to prod

--- script/utils/ConfigCore.sol
+++ script/utils/ConfigCore.sol
@@ -137,8 +137,8 @@ abstract contract ConfigCore is ConfigBase {
         // ===== PENDLE PT AMORTIZED ORACLE ADDRESSES =====
         // Staging: 0x1e6eC8e7a5aEe0508F43Deb35741F05E4a2e0004
         // Production: 0x5BDdD529B8ef20406371c3b864e0a78241d1dE0F
-        configuration.pendlePTAmortizedOracles[MAINNET_CHAIN_ID] = 0x1e6eC8e7a5aEe0508F43Deb35741F05E4a2e0004;
-        configuration.pendlePTAmortizedOracles[BASE_CHAIN_ID] = 0x1e6eC8e7a5aEe0508F43Deb35741F05E4a2e0004;
+        configuration.pendlePTAmortizedOracles[MAINNET_CHAIN_ID] = 0x5BDdD529B8ef20406371c3b864e0a78241d1dE0F;
+        configuration.pendlePTAmortizedOracles[BASE_CHAIN_ID] = 0x5BDdD529B8ef20406371c3b864e0a78241d1dE0F;
         configuration.pendlePTAmortizedOracles[BNB_CHAIN_ID] = address(0);
         configuration.pendlePTAmortizedOracles[ARBITRUM_CHAIN_ID] = address(0);
         configuration.pendlePTAmortizedOracles[OPTIMISM_CHAIN_ID] = address(0);
