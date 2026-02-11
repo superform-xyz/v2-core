2c43384f3683b698a938358c5eff4fe29a55324b
supervaulter <cosmin@superform.xyz>
2026-02-11 10:27:19 +0200

    chore: deployed a new version of RecordPurchasePendlePTAmortizedOracleHook

--- src/hooks/oracles/pendle/RecordPurchasePendlePTAmortizedOracleHook.sol
+++ src/hooks/oracles/pendle/RecordPurchasePendlePTAmortizedOracleHook.sol
@@ -18,7 +18,7 @@ import { IPendlePTAmortizedOracle } from "../../../vendor/pendle/IPendlePTAmorti
 /// @dev The strategy (msg.sender during execution) will be recorded as the position holder
 /// @dev data has the following structure
 /// @notice         address market = BytesLib.toAddress(data, 0);
-/// @notice         uint256 sySpent = BytesLib.toUint256(data, 20);
+/// @notice         uint256 syAccountingAssetSpent = BytesLib.toUint256(data, 20);
 /// @notice         uint256 ptAmount = BytesLib.toUint256(data, 52);
 /// @notice         bool usePrevHookAmount = _decodeBool(data, 84);
 contract RecordPurchasePendlePTAmortizedOracleHook is BaseHook, ISuperHookContextAware {
@@ -26,8 +26,11 @@ contract RecordPurchasePendlePTAmortizedOracleHook is BaseHook, ISuperHookContex
                                 CONSTANTS
     //////////////////////////////////////////////////////////////*/
 
+    /// @notice Contract version for bytecode differentiation
+    uint256 public constant VERSION = 2;
+
     uint256 private constant MARKET_POSITION = 0;
-    uint256 private constant SY_SPENT_POSITION = 20;
+    uint256 private constant SY_ACCOUNTING_ASSET_SPENT_POSITION = 20;
     uint256 private constant PT_AMOUNT_POSITION = 52;
     uint256 private constant USE_PREV_HOOK_AMOUNT_POSITION = 84;
 
@@ -73,7 +76,10 @@ contract RecordPurchasePendlePTAmortizedOracleHook is BaseHook, ISuperHookContex
         returns (Execution[] memory executions)
     {
         address market = BytesLib.toAddress(data, MARKET_POSITION);
-        uint256 sySpent = BytesLib.toUint256(data, SY_SPENT_POSITION);
+        /// @dev syAccountingAssetSpent is the amount of sy accounting asset spent on the purchase
+        ///      Usually the accounting asset can be obtained from SY token: `sy.assetInfo()`
+        ///      And SY token can be obtained from market: `market.readTokens()`
+        uint256 syAccountingAssetSpent = BytesLib.toUint256(data, SY_ACCOUNTING_ASSET_SPENT_POSITION);
         uint256 ptAmount = BytesLib.toUint256(data, PT_AMOUNT_POSITION);
         bool usePrevHookAmount = _decodeBool(data, USE_PREV_HOOK_AMOUNT_POSITION);
 
@@ -83,14 +89,14 @@ contract RecordPurchasePendlePTAmortizedOracleHook is BaseHook, ISuperHookContex
         }
 
         if (market == address(0)) revert MARKET_NOT_VALID();
-        if (sySpent == 0) revert SY_SPENT_NOT_VALID();
+        if (syAccountingAssetSpent == 0) revert SY_SPENT_NOT_VALID();
         if (ptAmount == 0) revert PT_AMOUNT_NOT_VALID();
 
         executions = new Execution[](1);
         executions[0] = Execution({
             target: address(ORACLE),
             value: 0,
-            callData: abi.encodeCall(IPendlePTAmortizedOracle.recordPurchase, (market, sySpent, ptAmount))
+            callData: abi.encodeCall(IPendlePTAmortizedOracle.recordPurchase, (market, syAccountingAssetSpent, ptAmount))
         });
     }
 
@@ -110,11 +116,11 @@ contract RecordPurchasePendlePTAmortizedOracleHook is BaseHook, ISuperHookContex
         return BytesLib.toAddress(data, MARKET_POSITION);
     }
 
-    /// @notice Decode the sySpent amount from hook data
+    /// @notice Decode the syAccountingAssetSpent amount from hook data
     /// @param data The hook data
-    /// @return The sySpent amount
+    /// @return The syAccountingAssetSpent amount
     function decodeSySpent(bytes memory data) external pure returns (uint256) {
-        return BytesLib.toUint256(data, SY_SPENT_POSITION);
+        return BytesLib.toUint256(data, SY_ACCOUNTING_ASSET_SPENT_POSITION);
     }
 
     /// @notice Decode the ptAmount from hook data
